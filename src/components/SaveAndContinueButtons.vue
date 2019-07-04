5198ed10a1a1b9b997153cc1dab0090f7f432045
Alex Bokii <vanilinka33@gmail.com>
2019-07-04 12:34:45 +0100

    Change 'Save as draft' test to 'Save' and change order
    Change 'Save as draft' button test to 'Save’, and move this updated
    ‘Save’ button before ‘Save and continue ’

--- src/components/SaveAndContinueButtons.vue
+++ src/components/SaveAndContinueButtons.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="form-actions">
+    <button class="btn btn-outline-secondary mr-2" :disabled="isSaving" type="submit">
+      Save
+    </button>
     <button class="btn btn-primary mr-2" type="button" :disabled="isSaving" @click.prevent="$emit('saveAndContinue')">
       Save and continue
-    </button>
-    <button class="btn btn-outline-secondary mr-2" :disabled="isSaving" type="submit">
-      Save as draft
     </button>
     <span class="spinner-border spinner-border-sm text-secondary" v-if="isSaving"></span>
   </div>
