3110ca56fb033d6d071ae778ad785214f331796c
Ollie Treend <ollie@treend.com>
2019-07-08 12:57:12 +0100

    Add Sentry to the application form
    I've integrated Sentry error tracking into the Vue app for application forms. In production, JavaScript exceptions which are thrown in end users' browsers are logged to Sentry.
    
    Errors are only logged to Sentry when Vue is built and running in a production environment.
    
    This is essentially a copy & paste of the Sentry + Vue integration used in the QT app. The main difference is that it uses a different Sentry DSN to log it to a different project.

--- src/main.js
+++ src/main.js
@@ -6,6 +6,14 @@ import store from '@/store';
 
 Vue.config.productionTip = false;
 
+if (process.env.NODE_ENV !== 'development') {
+  // Don't use Sentry in development
+  // We want to see Vue & JS errors in the local developer console rather than being silently rerouted to Sentry
+  // Related GitHub issues: https://github.com/getsentry/sentry-javascript/issues/1416 https://github.com/vuejs/vue/issues/8433
+  const sentry = require('@/sentry');
+  sentry.initSentry();
+}
+
 // Initialise the Vue app once Firebase Auth has initialised
 // The Vue app depends on the user's auth state to load the correct view (i.e. an authenticated view or the login view)
 // We only want to initialise Vue once, so we assign it to `vueInstance` and do nothing on subsequent calls
