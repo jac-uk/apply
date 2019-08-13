2ecd58806b61eb75cf9b065b7a0d1b97025e078a
Todd Tyree <tatyree@gmail.com>
2019-08-13 11:54:34 +0100

    Enable happy path email sign-in
    NOTE: This **does not** seem to allow display name collection at sign in time.

---
+++ src/components/core/FirebaseUI.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="firebaseui-auth-container" />
+</template>
+
+<script>
+import { auth } from '@/firebase';
+import * as firebaseui from 'firebaseui';
+
+export default {
+  name: 'FirebaseUI',
+  data() {
+    return {
+      uiConfig: {
+        signInOptions: [
+          {
+            provider: auth.EmailAuthProvider.PROVIDER_ID,
+            signInMethod: auth.EmailAuthProvider.EMAIL_LINK_SIGN_IN_METHOD,
+            requireDisplayName: true,
+          },
+        ],
+        credentialHelper: firebaseui.auth.CredentialHelper.NONE,
+        callbacks: {
+          signInSuccessWithAuthResult: this.signInSuccess,
+        },
+      },
+    };
+  },
+  mounted() {
+    this.ui = new firebaseui.auth.AuthUI(auth());
+    this.ui.start('#firebaseui-auth-container', this.uiConfig);
+  },
+  destroyed() {
+    this.ui.delete();
+  },
+  methods: {
+    signInSuccess(authResult) {
+      this.$emit('signInSuccess', authResult);
+      // Return false to disable FirebaseUI auth redirect
+      return false;
+    },
+  },
+};
+</script>
+
+<style src="firebaseui/dist/firebaseui.css"></style>
