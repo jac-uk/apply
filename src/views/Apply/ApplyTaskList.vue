4dab1b9200c773ec959c0d10d05d79d5512ef8cc
Lauren Qurashi <lauren.qurashi@me.com>
2019-10-10 12:45:43 +0100

    Added styling to the Tasklist page.

--- src/views/Apply/ApplyTaskList.vue
+++ src/views/Apply/ApplyTaskList.vue
@@ -1,11 +1,161 @@
 <template>
-  <div>
-    <h1>Apply for a role</h1>
-    <ul>
-      <li>Complete eligibility checklist</li>
-      <li>Add personal details</li>
-    </ul>
+  <div class="govuk-grid-row">
+    <div class="govuk-grid-column-two-thirds">
+      <span class="govuk-caption-xl govuk-!-padding-bottom-2 display-block">
+        042 Recorder
+      </span>
+
+      <h1 class="govuk-heading-xl">
+        Apply for the role
+      </h1>
+
+      <h2 class="govuk-heading-m">
+        1. Account Profile
+      </h2>
+      <ul class="govuk-list govuk-!-margin-bottom-9">
+        <li class="govuk-!-margin-bottom-0 govuk-!-padding-top-2 govuk-!-padding-bottom-2 container-border-bottom container-border-top">
+          <span class="govuk-body">
+            <a
+              class="govuk-link govuk-!-font-weight-bold"
+              href="#"
+            >
+              Add personal details
+            </a>
+          </span>
+        </li>
+        <li class="govuk-!-margin-bottom-0 govuk-!-padding-top-2 govuk-!-padding-bottom-2 container-border-bottom">
+          <span class="govuk-body">
+            <a
+              class="govuk-link govuk-!-font-weight-bold"
+              href="#"
+            >
+              Declare character information
+            </a>
+          </span>
+        </li>
+        <li class="govuk-!-margin-bottom-0 govuk-!-padding-top-2 govuk-!-padding-bottom-2 container-border-bottom">
+          <span class="govuk-body">
+            <a
+              class="govuk-link govuk-!-font-weight-bold"
+              href="#"
+            >
+              Take the equality and diversity survey
+            </a>
+          </span>
+        </li>
+      </ul>
+
+      <h2 class="govuk-heading-m">
+        2. Working preferences
+      </h2>
+      <ul class="govuk-list govuk-!-margin-bottom-9">
+        <li class="govuk-!-margin-bottom-0 govuk-!-padding-top-2 govuk-!-padding-bottom-2 container-border-bottom container-border-top">
+          <span class="govuk-body">
+            <a
+              class="govuk-link govuk-!-font-weight-bold"
+              href="#"
+            >
+              Set part-time working preferences
+            </a>
+          </span>
+        </li>
+      </ul>
+
+      <h2 class="govuk-heading-m">
+        3. Qualifications and experience
+      </h2>
+      <ul class="govuk-list govuk-!-margin-bottom-9">
+        <li class="govuk-!-margin-bottom-0 govuk-!-padding-top-2 govuk-!-padding-bottom-2 container-border-bottom container-border-top">
+          <span class="govuk-body">
+            <a
+              class="govuk-link govuk-!-font-weight-bold"
+              href="#"
+            >
+              Add relevant qualifications
+            </a>
+          </span>
+        </li>
+        <li class="govuk-!-margin-bottom-0 govuk-!-padding-top-2 govuk-!-padding-bottom-2 container-border-bottom">
+          <span class="govuk-body">
+            <a
+              class="govuk-link govuk-!-font-weight-bold"
+              href="#"
+            >
+              Add post-qualification work experience
+            </a>
+          </span>
+        </li>
+        <li class="govuk-!-margin-bottom-0 govuk-!-padding-top-2 govuk-!-padding-bottom-2 container-border-bottom">
+          <span class="govuk-body">
+            <a
+              class="govuk-link govuk-!-font-weight-bold"
+              href="#"
+            >
+              Add judicial experience
+            </a>
+          </span>
+        </li>
+      </ul>
+
+      <h2 class="govuk-heading-m">
+        4. Assessments
+      </h2>
+      <ul class="govuk-list govuk-!-margin-bottom-9">
+        <li class="govuk-!-margin-bottom-0 govuk-!-padding-top-2 govuk-!-padding-bottom-2 container-border-bottom container-border-top">
+          <span class="govuk-body">
+            <a
+              class="govuk-link govuk-!-font-weight-bold"
+              href="#"
+            >
+              Give independent assessors' details
+            </a>
+          </span>
+        </li>
+        <li class="govuk-!-margin-bottom-0 govuk-!-padding-top-2 govuk-!-padding-bottom-2 container-border-bottom">
+          <span class="govuk-body">
+            <a
+              class="govuk-link govuk-!-font-weight-bold"
+              href="#"
+            >
+              Upload self-assessment competencies
+            </a>
+          </span>
+        </li>
+      </ul>
+
+      <h2 class="govuk-heading-m">
+        5. Apply
+      </h2>
+      <ul class="govuk-list govuk-!-margin-bottom-9">
+        <li class="govuk-!-margin-bottom-0 govuk-!-padding-top-2 govuk-!-padding-bottom-2 container-border-bottom container-border-top">
+          <span class="govuk-body">
+            <a
+              class="govuk-link govuk-!-font-weight-bold"
+              href="#"
+            >
+              Review application
+            </a>
+          </span>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
-export default {}
+<script>
+export default {
+
+};
+</script>
+
+<style scoped>
+
+.status {
+  float: right;
+}
+
+.container-border-top {
+  border-top: 1px solid #b1b4b6
+}
+
+</style>
