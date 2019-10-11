0fa5f01ec415cf66579ceb68622f287eb67beca9
Lauren Qurashi <lauren.qurashi@me.com>
2019-10-11 13:26:54 +0100

    Added Post Qualifications Experience page
    - Created a repeatable fields component in a similar fashion to the 
    Qualification RF component. 
    
    - Amended the basic scaffold of the Post Qualification Wokr Experience 
    page to include the repeatable field component. 
    
    All code has been linted and tests are passing.

--- src/views/Apply/QualificationsAndExperience/PostQualificationWorkExperience.vue
+++ src/views/Apply/QualificationsAndExperience/PostQualificationWorkExperience.vue
@@ -1,5 +1,44 @@
 <template>
-  <h1>Add post-qualification work experience</h1>
+  <div class="govuk-grid-row">
+    <form @submit.prevent="save">
+      <div class="govuk-grid-column-two-thirds">
+        <h1 class="govuk-heading-xl">
+          Post-qualification experience
+        </h1>
+
+        <RepeatableFields
+          v-model="experience"
+          :component="repeatableFields.Experience"
+        />
+
+        <button class="govuk-button">
+          Continue
+        </button>
+      </div>
+    </form>
+  </div>
 </template>
 
-export default {}
+<script>
+import RepeatableFields from '@/components/RepeatableFields';
+import Experience from '@/components/RepeatableFields/Experience';
+
+export default {
+  components: {
+    RepeatableFields,
+  },
+  data(){
+    return {
+      repeatableFields: {
+        Experience,
+      },
+      experience: null,
+    };
+  },
+  methods: {
+    save() {
+
+    },
+  },
+};
+</script>
