cbe880204ddcc0a8ae9741d307e6c675b2290b46
Lauren Qurashi <lauren.qurashi@me.com>
2019-10-14 10:47:50 +0100

    Refactored the old (V1.2) Personal Details page
    - Amended the old personal details page to match the V2.1 styling.
    
    - Amended the tests too.
    
    All code has been linted and tests are passing.

--- src/views/Apply/AccountProfile/PersonalDetails.vue
+++ src/views/Apply/AccountProfile/PersonalDetails.vue
@@ -2,26 +2,41 @@
   <div class="govuk-grid-row">
     <form @submit.prevent="save">
       <div class="govuk-grid-column-two-thirds">
-        <span class="govuk-caption-xl govuk-!-padding-bottom-5 display-block">
-          092 Registrar of Criminal Appeals, Master of the Crown Office and Queen’s Coroner &amp;
-          Attorney
-        </span>
-
         <h1 class="govuk-heading-xl">
           Personal details
         </h1>
+
+        <p class="govuk-body-l">
+          We'll use this information to confirm your identity if you contact us.
+        </p>
 
         <TextField
           id="fullName"
           v-model="fullName"
           label="Full name"
+          hint="You do not need to include any titles."
         />
 
         <TextField
           id="email"
           v-model="email"
-          label="Email"
+          label="Email address"
           type="email"
+        />
+
+        <DateInput
+          id="date-of-birth"
+          v-model="dateOfBirth"
+          label="Date of birth"
+          hint="For example, 27 3 1964"
+        />
+
+        <TextField
+          id="national-insurance-number"
+          v-model="nationalInsuranceNumber"
+          label="National Insurance number"
+          hint="It’s on your National Insurance card, payslip or P60. For example, ‘QQ 12 34 56 C’."
+          class="govuk-!-width-one-half"
         />
 
         <button class="govuk-button">
@@ -34,15 +49,20 @@
 
 <script>
 import TextField from '@/components/Form/TextField';
+import DateInput from '@/components/Form/DateInput';
 
 export default {
   components: {
     TextField,
+    DateInput,
   },
   data(){
     return {
       fullName: null,
       email: null,
+      dateOfBirth: null,
+      nationalInsuranceNumber: null,
+
     };
   },
   methods: {
