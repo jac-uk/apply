59ae4aace6cc94a4982d0d12fe9c8294f18f6aba
Lauren Qurashi <laurenqurashi@me.com>
2019-10-22 14:32:27 +0100

    Added Sentry intergration

--- src/main.js
+++ src/main.js
@@ -3,12 +3,19 @@ import App from '@/App';
 import router from '@/router';
 import store from '@/store';
 import { auth } from '@/firebase';
+import * as Sentry from '@sentry/browser';
+import * as Integrations from '@sentry/integrations';
+
+Sentry.init({
+  dsn: 'https://2366ef9baa1a49bb8aa29c5262757de9@sentry.io/1499367',
+  integrations: [new Integrations.Vue({Vue, attachProps: true})],
+});
 Vue.config.productionTip = false;
 
 let vueInstance = false;
 auth().onAuthStateChanged( (user) => {
   store.dispatch('setCurrentUser', user);
-  
+
   if (!vueInstance) {
     vueInstance = new Vue({
       el: '#app',
