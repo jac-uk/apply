ac04088e31dd78805f9548e6b6f865055d50b42d
Lauren Qurashi <laurenqurashi@me.com>
2019-10-30 15:48:07 +0000

    Added functionality to switch titles of 3rd bulletP, based on role
    Added functionality that changes the test of the 3rd task list block, 
    depending on the type of role. 
    
    Tests have been written and are passing.
    
    code has been linted.

--- tests/unit/views/Apply/TaskList.spec.js
+++ tests/unit/views/Apply/TaskList.spec.js
@@ -1,47 +1,51 @@
 import TaskList from '@/views/Apply/TaskList';
 import { shallowMount } from '@vue/test-utils';
-
 const mockRoute = {
-  name: 'name-of-current-route',
-  params: {
-    id: 'abc123',
-  },
+ name: 'name-of-current-route',
+ params: {
+   id: 'abc123',
+ },
 };
-
 const mockRouter = {
-  replace: jest.fn(),
+ replace: jest.fn(),
 };
-
 const createTestSubject = () => {
-  return shallowMount(TaskList, {
-    mocks: {
-      $route: mockRoute,
-      $router: mockRouter,
-    },
-    stubs: {
-      'RouterLink': true,
-    },
-  });
+ return shallowMount(TaskList, {
+   mocks: {
+     $route: mockRoute,
+     $router: mockRouter,
+   },
+   stubs: {
+     'RouterLink': true,
+   },
+ });
 };
-
 describe('views/TaskList', () => {
-  let wrapper;
-  beforeEach(() => {
-    wrapper = createTestSubject();
-  });
-
-  describe('template', () => {
-    it('renders', () => {
-      expect(wrapper.exists()).toBe(true);
-    });
-
-    it('contains a <h1>', () => {
-      expect(wrapper.contains('h1')).toBe(true);
-    });
-
-    it('contains a router-link tag', () => {
-      expect(wrapper.find('RouterLink-stub').exists()).toBe(true);
-    });
-  });
-
+ let wrapper;
+ beforeEach(() => {
+   wrapper = createTestSubject();
+ });
+ describe('template', () => {
+   it('renders', () => {
+     expect(wrapper.exists()).toBe(true);
+   });
+   it('contains a <h1>', () => {
+     expect(wrapper.contains('h1')).toBe(true);
+   });
+   it('contains a router-link tag', () => {
+     expect(wrapper.find('RouterLink-stub').exists()).toBe(true);
+   });
+   describe('Experience h2', () => {
+     it('renders the 3rd block h2 as Qualifications and experience if the role is legal', () => {
+        wrapper.setData({ unknownVariable: 'legal' });
+        expect(wrapper.find('#qualifications-and-experience').exists()).toBe(true);
+        expect(wrapper.find('#qualifications-and-experience').text()).toBe('3. Qualifications and experience');
+      });
+      it('renders the 3rd block h2 as Memberships and experience if the role is non-legal', () => {
+         wrapper.setData({ unknownVariable: 'non-legal' });
+         expect(wrapper.find('#memberships-and-experience').exists()).toBe(true);
+         expect(wrapper.find('#memberships-and-experience').text()).toBe('3. Memberships and experience');
+       });
+   });
+ });
 });
