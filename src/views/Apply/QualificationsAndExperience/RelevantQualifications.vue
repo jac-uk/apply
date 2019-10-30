8fe2fcf4069c35b0749addf397be73d5df223281
Lauren Qurashi <laurenqurashi@me.com>
2019-10-30 14:25:56 +0000

    Added functionality to hide and change title depending on exercist type
    h1 of the page changes between qualifications and memberships depending 
    on legal/non-legal.
    
    Qualification RF component and Memberships component change depending on 
    type of exercise. 
    
    Tests have been written and are passing. 
    Code has been linted

--- src/views/Apply/QualificationsAndExperience/RelevantQualifications.vue
+++ src/views/Apply/QualificationsAndExperience/RelevantQualifications.vue
@@ -2,14 +2,37 @@
   <div class="govuk-grid-row">
     <form @submit.prevent="save">
       <div class="govuk-grid-column-two-thirds">
-        <h1 class="govuk-heading-xl">
+        <h1
+          v-if="unknownVariable !== 'non-legal'"
+          class="govuk-heading-xl"
+        >
           Qualifications
         </h1>
 
+        <h1
+          v-if="unknownVariable === 'non-legal'"
+          class="govuk-heading-xl"
+        >
+          Memberships
+        </h1>
+
         <RepeatableFields
+          v-if="unknownVariable !== 'non-legal'"
           v-model="qualifications"
           :component="repeatableFields.Qualification"
         />
+
+        <RadioGroup
+          v-if="unknownVariable === 'non-legal'"
+          id="professional-memberships"
+          v-model="professionalMemberships"
+          label="What professional memberships do you have?"
+        >
+          <RadioItem
+            value="option-a"
+            label="Option A"
+          />
+        </RadioGroup>
 
         <button class="govuk-button">
           Continue
@@ -22,10 +45,14 @@
 <script>
 import RepeatableFields from '@/components/RepeatableFields';
 import Qualification from '@/components/RepeatableFields/Qualification';
+import RadioItem from '@/components/Form/RadioItem';
+import RadioGroup from '@/components/Form/RadioGroup';
 
 export default {
   components: {
     RepeatableFields,
+    RadioItem,
+    RadioGroup,
   },
   data(){
     return {
@@ -33,6 +60,8 @@ export default {
         Qualification,
       },
       qualifications: null,
+      professionalMemberships: null,
+      unknownVariable: null,
     };
   },
   methods: {
@@ -42,4 +71,3 @@ export default {
   },
 };
 </script>
-
