e354e18e2c96a71072989533684a4c8de854da81
Warren Searle <warrensearle@Warrens-iMac.local>
2019-11-15 17:34:50 +0000

    Start saving Candidate data Also: * Change devServer port to 8181, so app can run alongside Admin * Package updates following fresh install * Include vuexfire

--- src/views/Apply/Apply.vue
+++ src/views/Apply/Apply.vue
@@ -1,5 +1,48 @@
 <template>
-  <RouterView />
+  <div>
+    <LoadingMessage
+      v-if="loaded === false"
+      :load-failed="loadFailed"
+    />
+    <RouterView v-else />
+  </div>
 </template>
 
-export default {}
+<script>
+import LoadingMessage from '@/components/LoadingMessage';
+
+export default {
+  components: {
+    LoadingMessage,
+  },
+  data() {
+    return {
+      loaded: false,
+      loadFailed: false,
+    };
+  },
+  mounted() {
+    const id = this.$route.params.id;
+    this.$store.dispatch('exercise/bind', id)
+      .then((data) => {
+        if(data === null) {
+          this.redirectToErrorPage();
+        } else {
+          this.loaded = true;
+          this.loadCandidateAndApplication();
+        }
+      }).catch((e) => {
+        this.loadFailed = true;
+        throw e;
+      });
+  },
+  methods: {
+    redirectToErrorPage() {
+      this.$router.replace({ name: 'exercise-not-found' });
+    },
+    loadCandidateAndApplication () {
+      this.$store.dispatch('candidate/bind', this.$store.state.auth.currentUser.uid);
+    },
+  },
+};
+</script>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 module.exports = {
+  devServer: {
+    port: 8181,
+  },
   css: {
     loaderOptions: {
       sass: {
