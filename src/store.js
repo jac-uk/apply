d37675e1aec923362531cae803460ec1939175aa
Warren Searle <warren@precise-minds.co.uk>
2019-11-18 12:22:37 +0000

    Start saving Candidate data Also: * Change devServer port to 8181, so app can run alongside Admin * Package updates following fresh install * Include vuexfire

--- src/store.js
+++ src/store.js
@@ -1,17 +1,25 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import auth from '@/store/auth';
-
+import { vuexfireMutations } from 'vuexfire';
 Vue.use(Vuex);
+
+// Vuex modules
+import auth from '@/store/auth';
+import exercise from '@/store/exercise';
+import candidate from '@/store/candidate';
 
 const store = new Vuex.Store({
   // Don't use strict mode in production for performance reasons (https://vuex.vuejs.org/guide/strict.html)
   strict: process.env.NODE_ENV !== 'production',
-
   modules: {
     auth,
+    exercise,
+    candidate,
   },
   state: {},
+  mutations: {
+    ...vuexfireMutations,
+  },
   actions: {},
   getters: {},
 });
