93c4cf5b78efeccc36eb540bf829ba20a0dd5b1a
Warren Searle <warrensearle@Warrens-iMac.local>
2019-11-25 14:36:53 +0000

    Improve candidate flow

---
+++ src/views/Vacancy/VacancyMessage.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="govuk-panel govuk-panel--confirmation background-blue text-left">
+    <h1 class="govuk-panel__title">
+      New application process
+    </h1>
+    <div class="govuk-panel__body">
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+      <p>Pellentesque elit eget gravida cum sociis natoque penatibus et magnis. </p>
+      <p>Fermentum posuere urna nec tincidunt praesent semper feugiat.</p>  
+      <button
+        class="govuk-button govuk-button--secondary"
+        data-module="govuk-button"
+      >
+        Continue
+      </button>      
+    </div>
+  </div>
+</template>
