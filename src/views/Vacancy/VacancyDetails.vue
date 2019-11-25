9b77c1ec1e2653852509a58c8d39d6e17c464ef4
Lauren Qurashi <laurenqurashi@me.com>
2019-11-25 15:59:58 +0000

    Added database reading functionality to the Vacancy details page
    - The Vacancy Details page is a replica of the job advert page - however this one spits out real data. 
    
    - Not all data is from the DB. Some is still hard coded such as nuber of vacancies and the overview about the role. 
    
    - Code has been linted and I've not broken any tests so far.

--- src/views/Vacancy/VacancyDetails.vue
+++ src/views/Vacancy/VacancyDetails.vue
@@ -1,41 +1,142 @@
 <template>
-  <div>
-    <div class="govuk-breadcrumbs">
-      <ol class="govuk-breadcrumbs__list">
-        <li class="govuk-breadcrumbs__list-item">
-          <RouterLink
-            class="govuk-breadcrumbs__link"
-            :to="{ name: 'vacancies' }"
-          >
-            Home
-          </RouterLink>
-        </li>
-        <li 
-          class="govuk-breadcrumbs__list-item"
-          aria-current="page"
+  <div class="govuk-grid-row">
+    <div class="govuk-grid-column-two-thirds">
+      <h1 class="govuk-heading-xl">
+        {{ vacancy.name }}
+      </h1>
+
+      <p><span class="govuk-body govuk-!-font-weight-bold">Number of vacancies:</span> <span class="govuk-body"> NEEDS CODING </span></p>
+      <p><span class="govuk-body govuk-!-font-weight-bold">Location:</span> <span class="govuk-body"> {{ vacancy.location }}</span></p>
+      <p><span class="govuk-body govuk-!-font-weight-bold">Salary:</span> <span class="govuk-body"> {{ vacancy.salaryGrouping }}</span></p>
+      <p class="govuk-!-margin-bottom-8">
+        <span class="govuk-body govuk-!-font-weight-bold">Contact:</span> <span class="govuk-body"> {{ vacancy.exerciseMailbox }}</span>
+      </p>
+
+      <h2 class="govuk-heading-l">
+        Timeline
+      </h2>
+      <Timeline :data="timeline" />
+
+      <h2 class="govuk-heading-l">
+        Overview of the role
+      </h2>
+
+      <p class="govuk-body">
+        As {{ vacancy.name }}... blah blah blah
+      </p>
+
+      <p class="govuk-body">
+        NEEDS CODING - bunch of stuff about what it takes to be a judge.
+      </p>
+
+      <h2 class="govuk-heading-l">
+        The selection process
+      </h2>
+
+      <p class="govuk-body">
+        There are 3 stages to the selection process.
+      </p>
+
+      <h3 class="govuk-heading-m">
+        1. Check if you're eligible
+      </h3>
+      <p class="govuk-body">
+        Check you meet all the eligibility requirements for this
+        role. It's not a guarantee you're eligible – it's to give you an idea of whether you
+        might be.
+      </p>
+
+      <h3 class="govuk-heading-m">
+        2. The application form
+      </h3>
+
+      <p class="govuk-body">
+        You'll need to complete your:
+      </p>
+      <ul class="govuk-list govuk-list--bullet">
+        <li>qualifications</li>
+        <li>work experience</li>
+        <li> self-assessment competency statements</li>
+        <li>independent assessors' details</li>
+      </ul>
+
+      <h3 class="govuk-heading-m">
+        3. Selection day
+      </h3>
+      <p class="govuk-body">
+        Shortlisted candidates are invited to selection days. These
+        allow us to meet you in person to assess your suitability for the role. If you’re selected, we’ll email you a
+        week before with
+        details about what you’ll need to prepare for the day.
+      </p>
+
+      <button
+        class="govuk-button"
+      >
+        Check if you're eligible and apply
+      </button>
+
+      <p class="govuk-body">
+        <a
+          class="govuk-link"
+          href="#"
+        >Or get email updates about this role</a>
+      </p>
+    </div>
+
+    <div class="govuk-grid-column-one-third govuk-!-padding-bottom-8 container-border-bottom">
+      <h2 class="govuk-heading-m">
+        Related content
+      </h2>
+      <p class="govuk-body">
+        <a
+          class="govuk-link"
+          href="https://www.judicialappointments.gov.uk/overview-selection-process"
+          target="_blank"
         >
-          Vacancies
-        </li>
-      </ol>
+          How we shortlist candidates
+        </a>
+      </p>
+
+      <p class="govuk-body">
+        <a
+          class="govuk-link"
+          href="https://www.judicialappointments.gov.uk/completing-your-self-assessment"
+          target="_blank"
+        >
+          How to write your self-assessment competency statements
+        </a>
+      </p>
+
+      <p class="govuk-body">
+        <a
+          class="govuk-link"
+          href="https://www.judicialappointments.gov.uk/references-guidance-candidates"
+          target="_blank"
+        >
+          How to choose your independent assessors
+        </a>
+      </p>
     </div>
-    <div>
-      {{ vacancy }}
-    </div>
-    <RouterLink
-      class="govuk-button"
-      data-module="govuk-button"
-      :to="{ name: 'vacancy-message' }"
-    >
-      Apply
-    </RouterLink>
   </div>
 </template>
 
 <script>
+import Timeline from '@/components/Page/Timeline';
+import createTimeline from '@/helpers/Timeline/createTimeline';
+import exerciseTimeline from '@/helpers/Timeline/exerciseTimeline';
+
 export default {
+  components: {
+    Timeline,
+  },
   computed: {
     vacancy () {
       return this.$store.state.exercise.record;
+    },
+    timeline() {
+      let timeline = exerciseTimeline(this.vacancy);
+      return createTimeline(timeline);
     },
   },
 };
