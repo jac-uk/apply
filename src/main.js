06286677ce14adb6c31d7e26fb191be93cc68151
Lauren Qurashi <laurenqurashi@me.com>
2019-11-25 18:05:03 +0000

    Added Pass or Fail routing logic to the eligibility checker
    - Added a method that redirects to the pass or fail page depending on the answers to the eligibility checker. 
    
    - Added Exercise titles to the top of the eligibiltiy pass and eligibility fail page. 
    
    - Tests are currently failing. Journey tests are not happy about authentication.

--- src/main.js
+++ src/main.js
@@ -3,13 +3,13 @@ import App from '@/App';
 import router from '@/router';
 import store from '@/store';
 import { auth } from '@/firebase';
-import * as Sentry from '@sentry/browser';
-import * as Integrations from '@sentry/integrations';
-
-Sentry.init({
-  dsn: 'https://2366ef9baa1a49bb8aa29c5262757de9@sentry.io/1499367',
-  integrations: [new Integrations.Vue({ Vue, attachProps: true })],
-});
+// import * as Sentry from '@sentry/browser';
+// import * as Integrations from '@sentry/integrations';
+//
+// Sentry.init({
+//   dsn: 'https://2366ef9baa1a49bb8aa29c5262757de9@sentry.io/1499367',
+//   integrations: [new Integrations.Vue({ Vue, attachProps: true })],
+// });
 Vue.config.productionTip = false;
 
 let vueInstance = false;
