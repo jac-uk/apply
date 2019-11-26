43d0e3b9bbe4961154973a331458717b13fd235e
Warren Searle <warren@precise-minds.co.uk>
2019-11-26 14:14:11 +0000

    Update vacancy message

--- src/views/Vacancy/VacancyMessage.vue
+++ src/views/Vacancy/VacancyMessage.vue
@@ -4,9 +4,19 @@
       New application process
     </h1>
     <div class="govuk-panel__body">
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-      <p>Pellentesque elit eget gravida cum sociis natoque penatibus et magnis. </p>
-      <p>Fermentum posuere urna nec tincidunt praesent semper feugiat.</p>  
+      <p>If you've applied with JAC before, you'll notice that the process for this role is slightly different.</p>
+      <p>You'll need to enter some personal details to create your new account profile, as well as professional information for your application.</p>
+      <p>
+        You can still 
+        <RouterLink
+          class="govuk-link text-white"
+          data-module="govuk-button"
+          :to="{ name: 'sign-in' }"
+        >
+          sign into your existing account
+        </RouterLink>  
+        to access anything you need for this application, but you cannot apply there.
+      </p>
       <RouterLink
         class="govuk-button govuk-button--secondary"
         data-module="govuk-button"
@@ -17,3 +27,9 @@
     </div>
   </div>
 </template>
+
+<style scoped>
+.text-white {
+  color: white;
+}
+</style>
