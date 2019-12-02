a64e8db0954e9f78e1e068d803a69dde2d1bf826
Martin R <martin.raymundo@judicialappointments.digital>
2019-12-02 12:46:15 +0100

    enable file upload of candidate CV

--- src/views/Apply/Assessments/LeadershipSuitability.vue
+++ src/views/Apply/Assessments/LeadershipSuitability.vue
@@ -64,9 +64,15 @@
           </h2>
           <input
             id="file-upload-2"
+            ref="cvFile"
             class="govuk-file-upload"
             type="file"
           >
+          <div>
+            <button @click="uploadFile('file-upload-2')">
+              Upload CV 
+            </button>
+          </div>           
         </div>
 
         <button class="govuk-button">
@@ -78,6 +84,7 @@
 </template>
 
 <script>
+import firebase from 'firebase';
 import RadioGroup from '@/components/Form/RadioGroup';
 import RadioItem from '@/components/Form/RadioItem';
 export default {
@@ -86,12 +93,82 @@ export default {
     RadioItem,
   },
   data(){
+    const defaults = {
+      cvUrl: null,
+    };
+    const data = this.$store.getters['application/data']();
+    const application = { ...defaults, ...data };
     return {
+      application: application,
     };
   },
   methods: {
     save() {
     },
+    uploadFile(elementId) {
+      //console.log('uploadFile function fired');
+      const file = document.querySelector(`#${elementId}`).files[0];
+      //console.log(file);
+
+      // These are the folder names set up in Firebase Storage
+      const folderNameMap = new Map([
+        ['file-upload-2', 'candidate-cvs'],
+      ]);
+
+      const folderName = folderNameMap.get(elementId);
+
+      // set the Firebase Storage file path and name here
+      // e.g. candidate-cvs/myFile.docx
+      const fileSavePath = `${folderName}/${file.name}`;
+
+      const storageRef = firebase.storage().ref();
+
+      let uploadTask = storageRef.child(fileSavePath).put(file);
+
+      // Listen for state changes, errors, and completion of the upload.
+      uploadTask
+        .on(firebase.storage.TaskEvent.STATE_CHANGED, // or 'state_changed'
+            (snapshot) => {
+              // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+              //let progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+              //console.log('Upload is ' + progress + '% done');
+              switch (snapshot.state) {
+              case firebase.storage.TaskState.PAUSED: // or 'paused'
+                //console.log('Upload is paused');
+                break;
+              case firebase.storage.TaskState.RUNNING: // or 'running'
+                //console.log('Upload is running');
+                break;
+              }
+            }, (error) => {
+
+              // A full list of error codes is available at
+              // https://firebase.google.com/docs/storage/web/handle-errors
+              switch (error.code) {
+              case 'storage/unauthorized':
+                // User doesn't have permission to access the object
+                break;
+
+              case 'storage/canceled':
+                // User canceled the upload
+                break;
+
+              case 'storage/unknown':
+                // Unknown error occurred, inspect error.serverResponse
+                break;
+              }
+            }, () => {
+              // Upload completed successfully, now we can get the download URL
+              uploadTask.snapshot.ref.getDownloadURL().then((downloadURL) => {
+                //console.log('File available at', downloadURL);
+                if (downloadURL.includes('candidate-cvs')) {
+                  this.application.cvUrl = downloadURL;
+                  this.$store.dispatch('application/save', this.application);
+                  alert('SUCCESS: file uploaded');
+                }
+              });
+            });
+    },      
   },
 };
 </script>
