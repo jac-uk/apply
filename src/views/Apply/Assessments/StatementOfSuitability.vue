eb9351924317939755ef79d0906157b3f5d916a9
Martin R <martin.raymundo@judicialappointments.digital>
2019-12-02 13:27:47 +0100

    enable file upload of candidate statement of suitability

--- src/views/Apply/Assessments/StatementOfSuitability.vue
+++ src/views/Apply/Assessments/StatementOfSuitability.vue
@@ -30,12 +30,12 @@
         </p>
 
         <p class="govuk-body">
-          Come back to this page to upload your finished statements.
+          Come back to this page to upload your statement of suitability.
         </p>
 
         <div class="govuk-form-group">
           <h2 class="govuk-heading-m">
-            Download assessments template
+            Download Statement Of Suitability template
           </h2>
 
           <a
@@ -43,19 +43,25 @@
             class="govuk-link govuk-body-m"
             href="#"
           >
-            assessments-template.doc
+            statement-of-suitability-template.doc
           </a>
         </div>
 
         <div class="govuk-form-group">
           <h2 class="govuk-heading-m">
-            Upload finished assessments
+            Upload Statement of Suitability
           </h2>
           <input
             id="file-upload-2"
+            ref="suitabilityStatementFile"
             class="govuk-file-upload"
             type="file"
           >
+          <div>
+            <button @click="uploadFile('file-upload-2')">
+              Upload Statement Of Suitability 
+            </button>
+          </div>            
         </div>
 
         <button class="govuk-button">
@@ -67,6 +73,7 @@
 </template>
 
 <script>
+import firebase from 'firebase';
 import RadioGroup from '@/components/Form/RadioGroup';
 import RadioItem from '@/components/Form/RadioItem';
 
@@ -76,14 +83,83 @@ export default {
     RadioItem,
   },
   data(){
+    const defaults = {
+      statementOfSuitabilityUrl: null,
+    };
+    const data = this.$store.getters['application/data']();
+    const application = { ...defaults, ...data };
     return {
-
+      application: application,
     };
   },
   methods: {
     save() {
 
     },
+    uploadFile(elementId) {
+      //console.log('uploadFile function fired');
+      const file = document.querySelector(`#${elementId}`).files[0];
+      //console.log(file);
+
+      // These are the folder names set up in Firebase Storage
+      const folderNameMap = new Map([
+        ['file-upload-2', 'candidate-suitability-statement'],
+      ]);
+
+      const folderName = folderNameMap.get(elementId);
+
+      // set the Firebase Storage file path and name here
+      // e.g. candidate-cvs/myFile.docx
+      const fileSavePath = `${folderName}/${file.name}`;
+
+      const storageRef = firebase.storage().ref();
+
+      let uploadTask = storageRef.child(fileSavePath).put(file);
+
+      // Listen for state changes, errors, and completion of the upload.
+      uploadTask
+        .on(firebase.storage.TaskEvent.STATE_CHANGED, // or 'state_changed'
+            (snapshot) => {
+              // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+              //let progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+              //console.log('Upload is ' + progress + '% done');
+              switch (snapshot.state) {
+              case firebase.storage.TaskState.PAUSED: // or 'paused'
+                //console.log('Upload is paused');
+                break;
+              case firebase.storage.TaskState.RUNNING: // or 'running'
+                //console.log('Upload is running');
+                break;
+              }
+            }, (error) => {
+
+              // A full list of error codes is available at
+              // https://firebase.google.com/docs/storage/web/handle-errors
+              switch (error.code) {
+              case 'storage/unauthorized':
+                // User doesn't have permission to access the object
+                break;
+
+              case 'storage/canceled':
+                // User canceled the upload
+                break;
+
+              case 'storage/unknown':
+                // Unknown error occurred, inspect error.serverResponse
+                break;
+              }
+            }, () => {
+              // Upload completed successfully, now we can get the download URL
+              uploadTask.snapshot.ref.getDownloadURL().then((downloadURL) => {
+                //console.log('File available at', downloadURL);
+                if (downloadURL.includes('candidate-suitability-statement')) {
+                  this.application.suitabilityStatementUrl = downloadURL;
+                  this.$store.dispatch('application/save', this.application);
+                  alert('SUCCESS: file uploaded');
+                }
+              });
+            });
+    },     
   },
 };
 </script>
