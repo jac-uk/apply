616d2e31507d9f261a58482bdb4803f239d8de16
Warren Searle <warren@precise-minds.co.uk>
2019-12-17 15:35:28 +0000

    Memberships, Experience, Employment Gaps forms

--- src/views/Apply/QualificationsAndExperience/RelevantExperience.vue
+++ src/views/Apply/QualificationsAndExperience/RelevantExperience.vue
@@ -1,9 +1,55 @@
 <template>
-  <p>[Relevant experience]</p>
+  <div class="govuk-grid-row">
+    <form @submit.prevent="save">
+      <div class="govuk-grid-column-two-thirds">
+        <BackLink />
+        <h1 class="govuk-heading-xl">
+          Relevant experience
+        </h1>
+
+        <RepeatableFields
+          v-model="application.experience"
+          :component="repeatableFields.NonLegalExperience"
+        />
+
+        <button class="govuk-button">
+          Continue
+        </button>
+      </div>
+    </form>
+  </div>
 </template>
 
 <script>
+import RepeatableFields from '@/components/RepeatableFields';
+import NonLegalExperience from '@/components/RepeatableFields/NonLegalExperience';
+import BackLink from '@/components/BackLink';
+
 export default {
+  components: {
+    RepeatableFields,
+    BackLink,
+  },
+  data(){
+    const defaults =  {
+      experience: null,
+    };
+    const data = this.$store.getters['application/data']();
+    const application = { ...defaults, ...data };
+    return {
+      application: application,
+      repeatableFields: {
+        NonLegalExperience,
+      },
+    };
+  },
+  methods: {
+    async save() {
+      this.application.progress.relevantExperience = true;
+      await this.$store.dispatch('application/save', this.application);
+      this.$router.push({ name: 'task-list' });
+    },
+  },
 
 };
 </script>
