9b78595b2467e5739df12fe7282f6b322b44ec42
Warren Searle <warren@precise-minds.co.uk>
2019-12-17 15:35:28 +0000

    Memberships, Experience, Employment Gaps forms

---
+++ src/components/RepeatableFields/NonLegalExperience.vue
@@ -0,0 +1,69 @@
+<template>
+  <div>
+    <TextField
+      :id="experienceJobTitle"
+      v-model="row.jobTitle"
+      label="Job title"
+    />
+
+    <TextField
+      :id="experienceOrgBusinessName"
+      v-model="row.orgBusinessName"
+      label="Organisation or business name"
+    />
+
+    <DateInput
+      :id="experienceStartDate"
+      v-model="row.startDate"
+      label="Start date"
+      type="month"
+    />
+
+    <DateInput
+      :id="experienceEndDate"
+      v-model="row.endDate"
+      label="End date"
+      hint="Leave this blank if you're currently in this role."
+      type="month"
+    />
+
+    <slot name="removeButton" />
+  </div>
+</template>
+
+<script>
+import DateInput from '@/components/Form/DateInput';
+import TextField from '@/components/Form/TextField';
+
+export default {
+  name: 'Experience',
+  components: {
+    DateInput,
+    TextField,
+  },
+  props: {
+    row: {
+      required: true,
+      type: Object,
+    },
+    index: {
+      required: true,
+      type: Number,
+    },
+  },
+  computed: {
+    experienceJobTitle() {
+      return `experience_job_title_${this.index}`;
+    },
+    experienceOrgBusinessName() {
+      return `experience_org_business_name_${this.index}`;
+    },
+    experienceStartDate() {
+      return `experience_start_date_${this.index}`;
+    },
+    experienceEndDate() {
+      return `experience_end_date_${this.index}`;
+    },
+  },
+};
+</script>
