c2a9bc7759dbaad4b9effe752c4bd71c5ac1d241
londonprojects <jeremy.suarez@judicialappointments.digital>
2019-12-17 18:14:11 +0000

    fixed issues with lint

--- src/views/Applications.vue
+++ src/views/Applications.vue
@@ -1,86 +1,121 @@
 <template>
-<div>
-  <RouterLink class="govuk-link" :to="{ name: 'vacancies' }">
-    Vacancies
-  </RouterLink>
+  <div>
+    <RouterLink
+      class="govuk-link"
+      :to="{ name: 'vacancies' }"
+    >
+      Vacancies
+    </RouterLink>
 
-  <div class="govuk-grid-row">
-    <!--SIDE NAV-->
-    <div class="govuk-grid-column-one-quarter">
-      <ul class="dwp-vertical-navigation">
-        <li>
-          <a aria-current="page" href="Applications?current=Applications">
-            Applications
-          </a>
-        </li>
-        <li>
-          <a href="PersonalDetails?current=PersonalDetails">
-            Personal details
-          </a>
-        </li>
-        <li>
-          <a href="DiversityInformation?current=DiversityInformation">
-            Diversity information
-          </a>
-        </li>
-        <li>
-          <a href="CharacterInformation?current=tab-CharacterInformation">
-            Character information
-          </a>
-        </li>
-      </ul>
-    </div>
-
-    <div class="govuk-grid-column-three-quarters">
-
-      <!--HEADER-->
-      <h1 class="govuk-heading-xl">Applications</h1>
-
-      <ul class="govuk-list">
-        <li v-for="application in records" :key="application.id" class="govuk-!-margin-top-7">
-          <span v-if="application.exerciseName && application.exerciseRef" class="govuk-caption-l">{{ application.exerciseRef }}<br></span>
-          <RouterLink class="govuk-link govuk-heading-m govuk-!-font-weight-bold govuk-!-margin-bottom-0" :to="{ name: 'task-list', params: { id: application.exerciseId } }">
-            <span v-if="application.exerciseName">{{ application.exerciseName }}</span>
-            <span v-else-if="application.exerciseRef">{{ application.exerciseRef }}</span>
-            <span v-else>{{ application.exerciseId }}</span>
-          </RouterLink>
-          Status: {{ application.status }}
-        </li>
-      </ul>
-
-      <!--GREY PANEL-->
-      <h2 class="govuk-heading-m">Current applications</h2>
-      <div class="govuk-panel--s govuk-panel--grey">
-        <p class="govuk-body-l">114 Judge of the First-tier Tribunal for England and
-          Wales</p>
-        <p class="govuk-body"><a href="#">Continue with
-            application</a></p>
+    <div class="govuk-grid-row">
+      <!--SIDE NAV-->
+      <div class="govuk-grid-column-one-quarter">
+        <ul class="dwp-vertical-navigation">
+          <li>
+            <a
+              aria-current="page"
+              href="Applications?current=Applications"
+            >
+              Applications
+            </a>
+          </li>
+          <li>
+            <a href="PersonalDetails?current=PersonalDetails">
+              Personal details
+            </a>
+          </li>
+          <li>
+            <a href="DiversityInformation?current=DiversityInformation">
+              Diversity information
+            </a>
+          </li>
+          <li>
+            <a href="CharacterInformation?current=tab-CharacterInformation">
+              Character information
+            </a>
+          </li>
+        </ul>
       </div>
-      <br><br>
 
-      <h2 class="govuk-heading-m">Previous applications</h2>
-      <!--TABLE-->
-      <table class="govuk-table">
-        <tr class="govuk-table__row">
-          <td class="govuk-table__cell"><a href="../../apply-pre-shortlisting//v1-2-court/form-personal.html">086
-              Circuit judge</a></td>
-          <td class="govuk-table__cell">Closed 17 February 2018</td>
-          <td class="govuk-table__cell">Selected</td>
-        </tr>
-        <tr class="govuk-table__row">
-          <td class="govuk-table__cell"><a href="../../apply-pre-shortlisting//v1-2-court/form-personal.html">086
-              Recorder</a></td>
-          <td class="govuk-table__cell">Closed 12 April 2017</td>
-          <td class="govuk-table__cell">Not selected</td>
-        </tr>
-        </tbody>
-      </table>
+      <div class="govuk-grid-column-three-quarters">
+        <!--HEADER-->
+        <h1 class="govuk-heading-xl">
+          Applications
+        </h1>
+
+        <ul class="govuk-list">
+          <li
+            v-for="application in records"
+            :key="application.id"
+            class="govuk-!-margin-top-7"
+          >
+            <span
+              v-if="application.exerciseName && application.exerciseRef"
+              class="govuk-caption-l"
+            >{{ application.exerciseRef }}<br></span>
+            <RouterLink
+              class="govuk-link govuk-heading-m govuk-!-font-weight-bold govuk-!-margin-bottom-0"
+              :to="{ name: 'task-list', params: { id: application.exerciseId } }"
+            >
+              <span v-if="application.exerciseName">{{ application.exerciseName }}</span>
+              <span v-else-if="application.exerciseRef">{{ application.exerciseRef }}</span>
+              <span v-else>{{ application.exerciseId }}</span>
+            </RouterLink>
+            Status: {{ application.status }}
+          </li>
+        </ul>
+
+        <!--GREY PANEL-->
+        <h2 class="govuk-heading-m">
+          Current applications
+        </h2>
+        <div class="govuk-panel--s govuk-panel--grey">
+          <p class="govuk-body-l">
+            114 Judge of the First-tier Tribunal for England and
+            Wales
+          </p>
+          <p class="govuk-body">
+            <a href="#">Continue with
+              application</a>
+          </p>
+        </div>
+        <br><br>
+
+        <h2 class="govuk-heading-m">
+          Previous applications
+        </h2>
+        <!--TABLE-->
+        <table class="govuk-table">
+          <tr class="govuk-table__row">
+            <td class="govuk-table__cell">
+              <a href="../../apply-pre-shortlisting//v1-2-court/form-personal.html">086
+                Circuit judge</a>
+            </td>
+            <td class="govuk-table__cell">
+              Closed 17 February 2018
+            </td>
+            <td class="govuk-table__cell">
+              Selected
+            </td>
+          </tr>
+          <tr class="govuk-table__row">
+            <td class="govuk-table__cell">
+              <a href="../../apply-pre-shortlisting//v1-2-court/form-personal.html">086
+                Recorder</a>
+            </td>
+            <td class="govuk-table__cell">
+              Closed 12 April 2017
+            </td>
+            <td class="govuk-table__cell">
+              Not selected
+            </td>
+          </tr>
+        </table>
 
       <!--END MAIN WRAPPER-->
+      </div>
     </div>
   </div>
-
-</div>
 </template>
 
 <script>
