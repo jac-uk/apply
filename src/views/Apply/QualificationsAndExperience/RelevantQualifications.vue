4bc1ab54d66550886861ec1d2bc5d4401ef3445f
Lauren Qurashi <laurenqurashi@me.com>
2020-01-10 11:46:53 +0000

    Emergency Commit!!!
    - Refactored the Qualifications page to include the two new questions. Needs conditional rendering

--- src/views/Apply/QualificationsAndExperience/RelevantQualifications.vue
+++ src/views/Apply/QualificationsAndExperience/RelevantQualifications.vue
@@ -4,183 +4,75 @@
       <div class="govuk-grid-column-two-thirds">
         <BackLink />
         <h1
-          v-if="unknownVariable !== 'non-legal'"
           class="govuk-heading-xl"
         >
           Qualifications
         </h1>
 
-        <h1
-          v-if="unknownVariable === 'non-legal'"
-          class="govuk-heading-xl"
+        <RadioGroup
+          id="applying-under-schedule-2-d"
+          v-model="application.applyingUnderSchedule2d"
+          label="Are you applying under Schedule 2(d)?"
         >
-          Memberships
-        </h1>
+          <span>
+            <a
+              class="govuk-link govuk-body"
+              href="http://www.legislation.gov.uk/ukpga/2007/15/schedule/2"
+              target="_blank"
+            >
+              http://www.legislation.gov.uk/ukpga/2007/15/schedule/2
+            </a>
+          </span>
+
+          <RadioItem
+            :value="true"
+            label="Yes"
+          >
+            <TextareaInput
+              v-model="application.yesApplyingUnderSchedule2d"
+              label="Explain how you've gained exeperience in law."
+            />
+          </RadioItem>
+          <RadioItem
+            :value="false"
+            label="No"
+          />
+        </RadioGroup>
+
+        <RadioGroup
+          id="applying-under-schedule-2-d"
+          v-model="application.applyingUnderSchedule2Three"
+          label="Are you applying under Schedule 2(3)?"
+        >
+          <span>
+            <a
+              class="govuk-link govuk-body"
+              href="http://www.legislation.gov.uk/ukpga/2007/15/schedule/2"
+              target="_blank"
+            >
+              http://www.legislation.gov.uk/ukpga/2007/15/schedule/2
+            </a>
+          </span>
+
+          <RadioItem
+            :value="true"
+            label="Yes"
+          >
+            <TextareaInput
+              v-model="application.yesApplyingUnderSchedule2Three"
+              label="Explain how you've gained exeperience in law."
+            />
+          </RadioItem>
+          <RadioItem
+            :value="false"
+            label="No"
+          />
+        </RadioGroup>
 
         <RepeatableFields
-          v-if="unknownVariable !== 'non-legal'"
           v-model="application.qualifications"
           :component="repeatableFields.Qualification"
         />
-
-        <RadioGroup
-          v-if="unknownVariable === 'non-legal'"
-          id="professional-memberships"
-          v-model="application.professionalMemberships"
-          label="What professional memberships do you have?"
-        >
-          <RadioItem
-            value="chartered-association-of-building-engineers"
-            label="Chartered Association of Building Engineers"
-          >
-            <TextField
-              v-model="application.charteredAssociationBuildingEngineersDate"
-              label="When did you become a member?"
-            />
-            <TextField
-              v-model="application.charteredAssociationBuildingEngineersNumber"
-              label="Membership number?"
-            />
-          </RadioItem>
-          <RadioItem
-            value="chartered-institute-of-building"
-            label="Chartered Institute of Building"
-          >
-            <TextField
-              v-model="application.charteredInstituteBuildingDate"
-              label="When did you become a member?"
-            />
-            <TextField
-              v-model="application.charteredInstituteBuildingNumber"
-              label="Membership number?"
-            />
-          </RadioItem>
-
-          <RadioItem
-            value="chartered-institute-of-environmental-health"
-            label="Chartered Institute of Environmental Health"
-          >
-            <TextField
-              v-model="application.charteredInstituteEnvironmentalHealthDate"
-              label="When did you become a member?"
-            />
-            <TextField
-              v-model="application.charteredInstituteEnvironmentalHealthNumber"
-              label="Membership number?"
-            />
-          </RadioItem>
-          <RadioItem
-            value="general-medical-council"
-            label="General Medical Council"
-          >
-            <RadioGroup
-              id="general-medical-council-conditional-registration"
-              v-model="application.GeneralMedicalCouncilConditionalRegistration"
-              label="Is your registration conditional?"
-            >
-              <RadioItem
-                :value="true"
-                label="Yes (if you do have this you might not be successful in this post)"
-              >
-                <DateInput
-                  id="gmc-yes-date"
-                  v-model="application.gmcConditionalYesDate"
-                  label="When did you become a member?"
-                />
-                <TextField
-                  v-model="application.gmcConditionalYesNumber"
-                  label="Membership number?"
-                />
-                <TextField
-                  v-model="application.gmcConditionalYesDetails"
-                  label="Give details of the conditions and dates"
-                />
-              </RadioItem>
-              <RadioItem
-                :value="false"
-                label="No"
-              >
-                <DateInput
-                  id="gmc-no-date"
-                  v-model="application.gmcConditionalNoDate"
-                  label="When did you become a member?"
-                />
-                <TextField
-                  v-model="application.gmcConditionalNoNumber"
-                  label="Membership number?"
-                />
-              </RadioItem>
-            </RadioGroup>
-          </RadioItem>
-          <RadioItem
-            value="royal-college-of-psychiatrists"
-            label="Royal College of Psychiatrists"
-          >
-            <RadioGroup
-              id="royal-college-of-psychiatrists-fellow-3-years"
-              v-model="application.royalCollegePsychiatristsFellow3Years"
-              label="You have been a member or fellow for 3 years?"
-            >
-              <RadioItem
-                :value="true"
-                label="Yes"
-              >
-                <TextField
-                  v-model="application.rcpYesDate"
-                  label="When did you become a member?"
-                />
-                <TextField
-                  v-model="application.rcpYesNumber"
-                  label="Membership number?"
-                />
-              </RadioItem>
-              <RadioItem
-                :value="false"
-                label="No (if less than 3 years you might not be successful in this post)"
-              >
-                <TextField
-                  v-model="application.rcpNoDate"
-                  label="When did you become a member?"
-                />
-                <TextField
-                  v-model="application.rcpNoNumber"
-                  label="Membership number?"
-                />
-              </RadioItem>
-            </RadioGroup>
-          </RadioItem>
-          <RadioItem
-            value="royal-institution-of-chartered-surveyors"
-            label="Royal Institution of Chartered Surveyors"
-          >
-            <TextField
-              v-model="application.royalInstitutionCharteredSurveyorsDate"
-              label="When did you become a member?"
-            />
-            <TextField
-              v-model="application.royalInstitutionCharteredSurveyorsNumber"
-              label="Membership number?"
-            />
-          </RadioItem>
-          <RadioItem
-            value="other"
-            label="Other"
-          >
-            <TextField
-              v-model="application.otherProfessionalMemberships"
-              label="Associations or Institutes"
-            />
-            <TextField
-              v-model="application.otherProfessionalMembershipsDate"
-              label="When did you become a member?"
-            />
-            <TextField
-              v-model="application.otherProfessionalMembershipsNumber"
-              label="Membership number?"
-            />
-          </RadioItem>
-        </RadioGroup>
-
         <button class="govuk-button">
           Continue
         </button>
@@ -194,8 +86,7 @@ import RepeatableFields from '@/components/RepeatableFields';
 import Qualification from '@/components/RepeatableFields/Qualification';
 import RadioItem from '@/components/Form/RadioItem';
 import RadioGroup from '@/components/Form/RadioGroup';
-import TextField from '@/components/Form/TextField';
-import DateInput from '@/components/Form/DateInput';
+import TextareaInput from '@/components/Form/TextareaInput';
 import BackLink from '@/components/BackLink';
 
 export default {
@@ -203,36 +94,16 @@ export default {
     RepeatableFields,
     RadioItem,
     RadioGroup,
-    TextField,
-    DateInput,
+    TextareaInput,
     BackLink,
   },
   data(){
     const defaults = {
       qualifications: null,
-      professionalMemberships: null,
-      charteredAssociationBuildingEngineersDate: null,
-      charteredAssociationBuildingEngineersNumber: null,
-      charteredInstituteBuildingDate: null,
-      charteredInstituteBuildingNumber: null,
-      charteredInstituteEnvironmentalHealthDate: null,
-      charteredInstituteEnvironmentalHealthNumber: null,
-      GeneralMedicalCouncilConditionalRegistration: null,
-      gmcConditionalYesDate: null,
-      gmcConditionalYesNumber: null,
-      gmcConditionalYesDetails: null,
-      gmcConditionalNoDate: null,
-      gmcConditionalNoNumber: null,
-      royalCollegePsychiatristsFellow3Years: null,
-      rcpYesDate: null,
-      rcpYesNumber: null,
-      rcpNoDate: null,
-      rcpNoNumber: null,
-      royalInstitutionCharteredSurveyorsDate: null,
-      royalInstitutionCharteredSurveyorsNumber: null,
-      otherProfessionalMemberships: null,
-      otherProfessionalMembershipsDate: null,
-      otherProfessionalMembershipsNumber: null,
+      applyingUnderSchedule2d: null,
+      yesApplyingUnderSchedule2d: null,
+      applyingUnderSchedule2Three: null,
+      yesApplyingUnderSchedule2Three: null,
     };
     const data = this.$store.getters['application/data']();
     const application = { ...defaults, ...data };
@@ -241,7 +112,6 @@ export default {
       repeatableFields: {
         Qualification,
       },
-      unknownVariable: 'non-legal',
     };
   },
   methods: {
