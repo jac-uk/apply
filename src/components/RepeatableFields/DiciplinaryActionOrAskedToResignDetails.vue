3c6c1d9da9ee06cdb0b6165aaebd6b9f9c08e761
Lauren Qurashi <laurenqurashi@me.com>
2020-01-20 15:16:53 +0000

    Recreated the Character Declaration page.
    added RF's for each question

---
+++ src/components/RepeatableFields/DiciplinaryActionOrAskedToResignDetails.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <DateInput
+      :id="`diciplinary_date_${index}`"
+      v-model="row.date"
+      label="Date of offence"
+    />
+    <TextField
+      :id="`diciplinary_title_${index}`"
+      v-model="row.title"
+      label="Findings or actions"
+    />
+    <TextareaInput
+      :id="`diciplinary_details_${index}`"
+      v-model="row.details"
+      label="Details"
+      rows="5"
+    />
+    <slot name="removeButton" />
+  </div>
+</template>
+
+<script>
+import DateInput from '@/components/Form/DateInput';
+import TextField from '@/components/Form/TextField';
+import TextareaInput from '@/components/Form/TextareaInput';
+
+export default {
+  name: 'DiciplinaryActionOrAskedToResignDetails',
+  components: {
+    DateInput,
+    TextField,
+    TextareaInput,
+  },
+  props: {
+    row: {
+      required: true,
+      type: Object,
+    },
+    index: {
+      required: true,
+      type: Number,
+    },
+  },
+};
+</script>
---
+++ src/components/RepeatableFields/OtherCharacterIssuesDetails.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <DateInput
+      :id="`other_issues_date_${index}`"
+      v-model="row.date"
+      label="Date of offence"
+    />
+    <TextField
+      :id="`other_issues_title_${index}`"
+      v-model="row.title"
+      label="Findings or actions"
+    />
+    <TextareaInput
+      :id="`other_issues_details_${index}`"
+      v-model="row.details"
+      label="Details"
+      rows="5"
+    />
+    <slot name="removeButton" />
+  </div>
+</template>
+
+<script>
+import DateInput from '@/components/Form/DateInput';
+import TextField from '@/components/Form/TextField';
+import TextareaInput from '@/components/Form/TextareaInput';
+
+export default {
+  name: 'InvolvedInProfessionalMisconductDetails',
+  components: {
+    DateInput,
+    TextField,
+    TextareaInput,
+  },
+  props: {
+    row: {
+      required: true,
+      type: Object,
+    },
+    index: {
+      required: true,
+      type: Number,
+    },
+  },
+};
+</script>
