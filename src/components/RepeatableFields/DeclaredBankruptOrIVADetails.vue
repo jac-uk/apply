daef586397a2dbd25e5edf1d1c4f97a53f2885aa
Lauren Qurashi <laurenqurashi@me.com>
2020-01-20 15:16:53 +0000

    Recreated the Character Declaration page.
    added RF's for each question

---
+++ src/components/RepeatableFields/DeclaredBankruptOrIVADetails.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <DateInput
+      :id="`bankrupt_or_iva_date_${index}`"
+      v-model="row.date"
+      label="Date of offence"
+    />
+    <TextField
+      :id="`bankrupt_or_iva_title_${index}`"
+      v-model="row.title"
+      label="Penalty or fine"
+    />
+    <TextareaInput
+      :id="`bankrupt_or_iva_details_${index}`"
+      v-model="row.details"
+      label="Details"
+      rows="5"
+    />
+    <slot name="removeButton" />
+  </div>
+</template>
+
+<script>
+import DateInput from '@/components/Form/DateInput';
+import TextField from '@/components/Form/TextField';
+import TextareaInput from '@/components/Form/TextareaInput';
+
+export default {
+  name: 'DeclaredBankruptOrIVADetails',
+  components: {
+    DateInput,
+    TextField,
+    TextareaInput,
+  },
+  props: {
+    row: {
+      required: true,
+      type: Object,
+    },
+    index: {
+      required: true,
+      type: Number,
+    },
+  },
+};
+</script>
---
+++ src/components/RepeatableFields/LateTaxReturnOrFinedDetails.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <DateInput
+      :id="`endorsement_offence_date_${index}`"
+      v-model="row.date"
+      label="Date of offence"
+    />
+    <TextField
+      :id="`endorsement_offence_title_${index}`"
+      v-model="row.title"
+      label="Penalty or fine"
+    />
+    <TextareaInput
+      :id="`endorsement_offence_details_${index}`"
+      v-model="row.details"
+      label="Details"
+      rows="5"
+    />
+    <slot name="removeButton" />
+  </div>
+</template>
+
+<script>
+import DateInput from '@/components/Form/DateInput';
+import TextField from '@/components/Form/TextField';
+import TextareaInput from '@/components/Form/TextareaInput';
+
+export default {
+  name: 'LateTaxReturnOrFinedDetails',
+  components: {
+    DateInput,
+    TextField,
+    TextareaInput,
+  },
+  props: {
+    row: {
+      required: true,
+      type: Object,
+    },
+    index: {
+      required: true,
+      type: Number,
+    },
+  },
+};
+</script>
