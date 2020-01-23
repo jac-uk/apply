2b0c6de98539ecc9b855687930cc983c9a91dd52
Warren Searle <warren@precise-minds.co.uk>
2020-01-23 23:27:04 +0000

    Assessment Options. Add Statement of Eligibility

---
+++ src/views/Apply/Assessments/StatementOfEligibility.vue
@@ -0,0 +1,118 @@
+<template>
+  <div class="govuk-grid-row">
+    <form @submit.prevent="save">
+      <div class="govuk-grid-column-two-thirds">
+        <BackLink />
+        <h1 class="govuk-heading-xl">
+          Statement of eligibility
+        </h1>
+
+        <div v-if="vacancy.aSCApply && vacancy.selectionCriteria">
+          <div
+            v-for="(item, index) in application.selectionCriteriaAnswers"
+            :key="index"
+          >
+            <p
+              class="govuk-heading-m govuk-!-font-weight-bold"
+            >
+              {{ item.title }}
+            </p>
+            <p class="govuk-body">
+              {{ item.text }}
+            </p>
+
+            <RadioGroup
+              :id="`meet_requirements_${index}`"
+              v-model="item.answer"
+              label="Do you meet this requirement?"
+            >
+              <RadioItem
+                :value="true"
+                label="Yes"
+              >
+                <TextareaInput
+                  :id="`meet_requirements_details${index}`"
+                  v-model="item.answerDetails"
+                  label="In 250 words, tell us how."
+                />
+              </RadioItem>
+              <RadioItem
+                :value="false"
+                label="No"
+              />
+            </RadioGroup>
+          </div>
+        </div>
+
+        <button
+          :disabled="application.status != 'draft'"
+          class="govuk-button"
+        >
+          Save and continue
+        </button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+import RadioGroup from '@/components/Form/RadioGroup';
+import RadioItem from '@/components/Form/RadioItem';
+import TextareaInput from '@/components/Form/TextareaInput';
+import BackLink from '@/components/BackLink';
+import uploadMixin from '@/mixins/uploadMixin';
+
+export default {
+  components: {
+    RadioGroup,
+    RadioItem,
+    TextareaInput,
+    BackLink,
+  },
+  mixins: [uploadMixin],
+  data(){
+    const defaults = {
+      selectionCriteriaAnswers: [],
+    };
+    const data = this.$store.getters['application/data']();
+    const application = { ...defaults, ...data };
+    if (application.selectionCriteriaAnswers.length === 0) {
+      const vacancy = this.$store.state.vacancy.record;
+      if (vacancy && vacancy.aSCApply && vacancy.selectionCriteria) {
+        for (let i = 0, len = vacancy.selectionCriteria.length; i < len; ++i) {
+          application.selectionCriteriaAnswers.push({
+            title: vacancy.selectionCriteria[i].title,
+            text: vacancy.selectionCriteria[i].text,
+            answer: null,
+            answerDetails: null,
+          });
+        }
+      }
+    }
+    return {
+      application: application,
+      files: {},
+    };
+  },
+  computed: {
+    userId() {
+      return this.$store.state.auth.currentUser.uid;
+    },
+    vacancy() {
+      return this.$store.state.vacancy.record;
+    },
+  },
+  methods: {
+    async save() {
+      // loop through this.files and upload them
+      const files = Object.values(this.files);
+      for (const file of files) {
+        await this.upload(file);
+      }
+      this.application.progress.statementOfEligibility = true;
+      await this.$store.dispatch('application/save', this.application);
+      this.$router.push({ name: 'task-list' });
+    },
+  },
+};
+</script>
