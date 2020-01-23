bd37ffbc1b6b967d054126622404c57eab7f75cc
Lauren Qurashi <laurenqurashi@me.com>
2020-01-23 14:54:17 +0000

    Added LawRelatedTasks to the Experience component

--- src/components/RepeatableFields/Experience.vue
+++ src/components/RepeatableFields/Experience.vue
@@ -27,86 +27,27 @@
       type="month"
     />
 
-    <CheckboxGroup
-      :id="experienceTasks"
-      v-model="row.tasks"
-      label="Law-related tasks in this role"
-    >
-      <p class="govuk-hint govuk-!-margin-top-0">
-        Select all tasks you do in this role. These are defined in the
-        <a
-          class="govuk-link"
-          href="http://www.legislation.gov.uk/ukpga/2007/15/section/52"
-          target="_blank"
-        >
-          Tribunals, Courts and Enforcement Act 2007.
-        </a>
-      </p>
-
-      <CheckboxItem
-        value="The carrying-out of judicial functions of any court or tribunal"
-        label="The carrying-out of judicial functions of any court or tribunal"
-      />
-      <CheckboxItem
-        value="Acting as an arbitrator"
-        label="Acting as an arbitrator"
-      />
-      <CheckboxItem
-        value="Practice or employment as a lawyer"
-        label="Practice or employment as a lawyer"
-      />
-      <CheckboxItem
-        value="Advising on the application of the law"
-        label="Advising on the application of the law"
-      />
-      <CheckboxItem
-        value="Assisting persons involved in proceedings for the resolution of issues arising under the law"
-        label="Assisting persons involved in proceedings for the resolution of issues arising under the law"
-      />
-      <CheckboxItem
-        value="Acting as mediator in connection with attempts to resolve issues that are, or if not resolved could be, the subject of proceedings"
-        label="Acting as mediator in connection with attempts to resolve issues that are, or if not resolved could be, the subject of proceedings"
-      />
-      <CheckboxItem
-        value="Drafting documents intended to affect persons' rights or obligations"
-        label="Drafting documents intended to affect persons' rights or obligations"
-      />
-      <CheckboxItem
-        value="Teaching or researching law"
-        label="Teaching or researching law"
-      />
-      <CheckboxItem
-        value="None of the above - Any activity that, in the relevant decision-maker's opinion, is of a broadly similar nature to those listed above"
-        label="None of the above - Any activity that, in the relevant decision-maker's opinion, is of a broadly similar nature to those listed above"
-      >
-        <TextareaInput
-          :id="experienceOtherTasks"
-          v-model="row.otherTasks"
-          label="List your different law-related tasks"
-          rows="2"
-        />
-      </CheckboxItem>
-    </CheckboxGroup>
+    <LawRelatedTasks
+      :id="`tasks_${index}`"
+      :tasks.sync="row.tasks"
+      :otherTasks.sync="row.otherTasks"
+    />
 
     <slot name="removeButton" />
   </div>
 </template>
 
 <script>
-import CheckboxGroup from '@/components/Form/CheckboxGroup';
-import CheckboxItem from '@/components/Form/CheckboxItem';
 import DateInput from '@/components/Form/DateInput';
 import TextField from '@/components/Form/TextField';
-import TextareaInput from '@/components/Form/TextareaInput';
+import LawRelatedTasks from '@/components/Page/LawRelatedTasks';
 
 export default {
   name: 'Experience',
   components: {
-    CheckboxGroup,
-    CheckboxItem,
     DateInput,
     TextField,
-    TextareaInput,
+    LawRelatedTasks,
   },
   props: {
     row: {
