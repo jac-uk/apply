79514b4a56b3234d7be5067fab4179f9a6a68e94
YaaL <YaaL@users.noreply.github.com>
2020-03-03 09:57:38 +0000

    Added ability to reset password
    + updated deprecated jest config variable it was complaining about

---
+++ src/views/ResetPassword.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="govuk-grid-row">
+    <div class="govuk-grid-column-full">
+      <form @submit.prevent="resetPassword">
+        <div class="govuk-grid-column-two-thirds">
+          <h1 class="govuk-heading-xl">
+            Forgotten password
+          </h1>
+
+          <p class="govuk-body-l">
+            We'll email you a link to reset your password.
+          </p>
+
+          <ErrorSummary :errors="errors" />
+
+          <TextField
+            id="email"
+            v-model="formData.email"
+            label="Email address"
+            type="email"
+          />
+
+          <button class="govuk-button">
+            Send the link
+          </button>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+import ErrorSummary from '@/components/Form/ErrorSummary';
+import TextField from '@/components/Form/TextField';
+import { auth } from '@/firebase';
+
+export default {
+  components: {
+    ErrorSummary,
+    TextField,
+  },
+  data () {
+    return {
+      formData: {},
+      errors: [],
+    };
+  },
+  methods: {
+    resetPassword() {
+      const returnUrl = location.origin + this.$router.resolve({ name: 'sign-in' }).route.fullPath;
+
+      if (this.formData.email) {
+        this.errors = [];
+        auth().sendPasswordResetEmail(this.formData.email, {
+          url: returnUrl,
+        })
+          .then(() => {
+            this.$router.push({ name: 'sign-in' });
+          })
+          .catch((error) => {
+            // TODO: if user doesn't exist, message user and prompt to create account
+            this.errors.push({ id: 'email', message: error.message });
+          });
+      }
+    },
+  },
+};
+</script>
