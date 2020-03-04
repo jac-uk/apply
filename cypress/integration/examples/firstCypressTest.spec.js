c688f61847bb5d86b8e87bc1168ecaf113de7635
gabriel <gabriel.matos@judicialappointments.digital>
2020-03-04 15:05:50 +0000

    Fixed test errors, still incomplete

---
+++ cypress/integration/examples/firstCypressTest.spec.js
@@ -0,0 +1,134 @@
+/// <reference types="cypress" />
+
+context('Actions', () => {
+  beforeEach(() => {
+
+    cy.wait(400)
+
+
+    // cy.get($body).then($body) => {
+    // if ($body.find('.govuk-\!-margin-right-1').length == 1 ) {
+    //   cy.get('.govuk-\!-margin-right-1').click()
+    // }
+  })
+
+  it('user is able to sign in', () => {
+    cy.visit('https://apply-staging.judicialappointments.digital/sign-in')
+    cy.get('#email').type('test123@gmail.com')
+    cy.get('.govuk-grid-column-two-thirds > .govuk-button').click()
+    cy.get('#password').type('123456')
+    cy.get('.govuk-grid-column-two-thirds > .govuk-button').click()
+    cy.get('.govuk-heading-xl').should('have.text', ' Applications ') //assertion
+  })
+
+  it('user is able to apply to legal vacancy', () => {
+    cy.get(':nth-child(1) > .govuk-link').click()
+    cy.get(':nth-child(2) > :nth-child(1) > .govuk-link').click()
+    cy.get('.govuk-grid-column-two-thirds > .govuk-button').click()
+    cy.get('.govuk-panel__body > .govuk-button').click()
+    cy.get('#citizenship__1').click()
+    cy.get('.govuk-grid-column-two-thirds > .govuk-button').click()
+    cy.get('.govuk-grid-column-two-thirds > .govuk-button').click()
+    cy.get(':nth-child(1) > .govuk-list > .container-border-top > .govuk-grid-row > .govuk-grid-column-three-quarters > .govuk-link').click()
+    cy.get('#fullName').type('Test')
+    cy.get('#fullName').type('test123@gmail.com')
+    cy.get('#date-of-birth-day').type('27')
+    cy.get('#date-of-birth-month').type('3')
+    cy.get('#date-of-birth-year').clear().type('1964')
+    cy.get('#national-insurance-number').type('QQ 12 34 56 C')
+    cy.get('#citizenship__3').click()
+    cy.get('#reasonable-adjustments__8').click()
+    cy.get('.govuk-grid-column-two-thirds > .govuk-button').click()
+    cy.get(':nth-child(1) > .govuk-list > :nth-child(2) > .govuk-grid-row > .govuk-grid-column-three-quarters > .govuk-link').click()
+    cy.get('.govuk-grid-column-two-thirds > .govuk-button').click()
+    cy.get(':nth-child(1) > .govuk-list > :nth-child(3) > .govuk-grid-row > .govuk-grid-column-three-quarters > .govuk-link').click()
+    cy.get('#current-legal-role__23')
+    cy.get('#fee-paid-judicial-role__80').click()
+    cy.get('#state-or-fee-school__84').click()
+    cy.get('#oxbridge-universities__91').click()
+    cy.get('#first-generation-student__94').click()
+    cy.get('#ethnic-group__97').click()
+    cy.get('#gender__115').click()
+    cy.get('#changed-gender__120').click()
+    cy.get('#sexual-orientation__126').click()
+    cy.get('#disability__130').click()
+    cy.get('#religion-faith__132').click()
+    cy.get('#atttended-outreach-events__143').click()
+    cy.get('#participated-in-judicial-workshadowing-scheme__145').click()
+    cy.get('#has-taken-paje__148').click()
+    cy.get('.govuk-grid-column-two-thirds > .govuk-button').click()
+    cy.get(':nth-child(2) > .govuk-list > .container-border-top > .govuk-grid-row > .govuk-grid-column-three-quarters > .govuk-link').click()
+    cy.get('#location-preferences-answer-0').click()
+    cy.get('.govuk-grid-column-two-thirds > .govuk-button').click()
+    cy.get(':nth-child(2) > .govuk-list > :nth-child(2) > .govuk-grid-row > .govuk-grid-column-three-quarters > .govuk-link').click()
+    cy.get('#jurisdiction-preferences-answer-0').click()
+    cy.get('.govuk-grid-column-two-thirds > .govuk-button').click()
+    cy.get('.govuk-grid-column-two-thirds > .govuk-button').click()
+    cy.get('#qualification_type_0__3').click()
+    cy.get('#qualification_location_0__7').click()
+    cy.get('#qualification_date_0-month').type('01')
+    cy.get('#qualification_date_0-month').type('2020')
+    cy.get('.govuk-grid-column-two-thirds > :nth-child(4)').click()
+    cy.get(':nth-child(3) > .govuk-list > :nth-child(2) > .govuk-grid-row > .govuk-grid-column-three-quarters > .govuk-link').click()
+    cy.get('#experience_job_title_0').type('Beast mode')
+    cy.get('#experience_org_business_name_0').type('Supreme Court master')
+    cy.get('#experience_start_date_0-month').type('01')
+    cy.get('#experience_start_date_0-year').type('2020')
+    cy.get('#experience_end_date_0-month').type('02')
+    cy.get('#experience_end_date_0-year').type('2020')
+    cy.get('#tasks_0__1').click()
+    cy.get('.govuk-grid-column-two-thirds > :nth-child(5)').click()
+    cy.get(':nth-child(3) > .govuk-list > :nth-child(3) > .govuk-grid-row > .govuk-grid-column-three-quarters > .govuk-link').click()
+    cy.get('#fee-or-salaried-judge__12').click()
+    cy.get(':nth-child(1) > .govuk-radios__item > #fee-or-salaried-sat-thirty-days__17').click()
+    cy.get('#fee-or-salaried-sitting-days-details').type('10')
+    cy.get('#quasi-judicial-sat-thirty-days__20').click()
+    cy.get('#quasi-judicial-sitting-days-details').type(10)
+    cy.get('.govuk-grid-column-two-thirds > .govuk-button').click()
+    cy.get(':nth-child(4) > .govuk-grid-row > .govuk-grid-column-three-quarters > .govuk-link').click()
+    cy.get('#tasks_0__28').click()
+    cy.get(':nth-child(5) > .govuk-grid-row > .govuk-grid-column-three-quarters > .govuk-link').click()
+    cy.get('.govuk-grid-column-two-thirds > :nth-child(5)').click()
+    cy.get('.govuk-grid-column-two-thirds > .govuk-button').click()
+    cy.get('#first-assessor-full-name').type('test')
+    cy.get('#first-assessor-title').type('test')
+    cy.get('#first-assessor-email').type('test@gmail.com')
+    cy.get('#first-assessor-Phone').type('test')
+    cy.get('#second-assessor-full-name').type('test')
+    cy.get('#second-assessor-title').type('test')
+    cy.get('#second-assessor-email').type('test@gmail.com')
+    cy.get('#second-assessor-Phone').type('test')
+    cy.get('.govuk-grid-column-two-thirds > .govuk-button').click()
+    cy.get('.govuk-grid-column-two-thirds > .govuk-button').click()
+    cy.get('.govuk-grid-column-full > .govuk-button').click()
+
+
+
+
+
+
+
+  })
+
+  // it('user is able to apply to non legal vacancy', () => {
+  //   cy.get('.govuk-grid-column-two-thirds > .govuk-button').click()
+  //   cy.get('#password').type('123456')
+  //   cy.get('.govuk-grid-column-two-thirds > .govuk-button').click()
+  //   cy.get('.govuk-heading-xl').should('have.text', ' Applications ')
+  // })
+  //
+  // it('user is able to apply to leadership vacancy', () => {
+  //   cy.get('.govuk-grid-column-two-thirds > .govuk-button').click()
+  //   cy.get('#password').type('123456')
+  //   cy.get('.govuk-grid-column-two-thirds > .govuk-button').click()
+  //   cy.get('.govuk-heading-xl').should('have.text', ' Applications ')
+  // })
+  //
+  // it('user is able to apply to leadership non legal vacancy', () => {
+  //   cy.get('.govuk-grid-column-two-thirds > .govuk-button').click()
+  //   cy.get('#password').type('123456')
+  //   cy.get('.govuk-grid-column-two-thirds > .govuk-button').click()
+  //   cy.get('.govuk-heading-xl').should('have.text', ' Applications ')
+  // })
+
+})
