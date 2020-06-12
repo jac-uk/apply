3005b19280e3218956c869857e61665a0a8558c6
Tom Russell <TR115251@hotmail.co.uk>
2020-06-12 12:06:20 +0100

    more details page template

--- src/views/Apply/CharacterChecks/MoreDetails.vue
+++ src/views/Apply/CharacterChecks/MoreDetails.vue
@@ -7,30 +7,49 @@
       Back
     </RouterLink>
 
-    <div class="govuk-!-margin-top-8">
-      <h1 class="govuk-heading-xl">
-        Further information to disclose (optional)
-      </h1>
+    <div class="govuk-grid-row">
+      <div class="govuk-form-group">
+        <div class="govuk-grid-column-two-thirds govuk-!-margin-top-8">
+          <legend class="govuk-fieldset__legend govuk-fieldset__legend--xl">
+            <h1 class="govuk-fieldset__heading govuk-!-margin-bottom-4">
+              Further information to disclose (optional)
+            </h1>
+          </legend>
+          <TextareaInput
+            id="further-info"
+            ref="textareaInput"
+            class="govuk-heading-m govuk-!-margin-bottom-2"
+            rows="10"
+            hint="For example, misuse of social media or online networking sites, breaches of the Integrity Statement etc."
+            aria-describedby="more-details-info more-details-hint"
+          />
+          <span
+            id="more-details-info"
+            aria-live="polite"
+            class="govuk-hint govuk-character-count__message"
+          >You have {{ charsRemaining }} characters remaining</span>
+          <button
+            class="govuk-button govuk-!-margin-top-4"
+            data-module="govuk-button"
+          >
+            Save and continue
+          </button>
+        </div>
+      </div>
     </div>
-    <RadioGroup>
-      <RadioItem />
-    </RadioGroup>
-    <DateInput />
-    <TextField />
   </div>
 </template>
 <script>
-import { RadioItem } from '@/components/Form/RadioItem';
-import { RadioGroup } from '@/components/Form/RadioGroup';
-import { DateInput } from '@/components/Form/DateInput';
-import { TextField } from '@/components/Form/TextField';
+import TextareaInput from '@/components/Form/TextareaInput';
 
 export default {
   components: {
-    RadioItem,
-    RadioGroup,
-    DateInput,
-    TextField,
+    TextareaInput,
+  },
+  data() {
+    return {
+      charsRemaining: 1000,
+    };
   },
 };
     
