cc9e23820367f840a434cf91ce87df1b524a5e0d
Tom Russell <TR115251@hotmail.co.uk>
2020-07-14 11:49:27 +0100

    PR comment fixes

--- tests/unit/components/Form/Form.spec.js
+++ tests/unit/components/Form/Form.spec.js
@@ -1,14 +1,14 @@
 import { createTestSubject } from '../../helpers';
 
 import Form from '@/components/Form/Form';
-
+//@FIXME@ Testing this suite requires the ref 'formRef' to be mocked
 xdescribe('components/Form/Form', () => {
     describe('component instance', () => {  
         let wrapper;
         beforeEach(() => {
         wrapper = createTestSubject(Form, {
             mocks: {},
-            stubs: ['$el'],
+            stubs: [],
             propsData: {},
             });
         });
