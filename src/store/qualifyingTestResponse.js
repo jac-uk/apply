782911fc845b943e04961dc2cea0f28aae3ec385
YaaL <YaaL@users.noreply.github.com>
2020-08-20 18:47:24 +0100

    qualifyingTestResponse store

---
+++ src/store/qualifyingTestResponse.js
@@ -0,0 +1,29 @@
+import { firestore } from '@/firebase';
+import { firestoreAction } from 'vuexfire';
+import vuexfireSerialize from '@/helpers/vuexfireSerialize';
+
+const collection = firestore.collection('qualifyingTestResponses');
+
+export default {
+  namespaced: true,
+  actions: {
+    bind: firestoreAction(({ bindFirestoreRef }, id) => {
+      const firestoreRef = collection.doc(id);
+
+      return bindFirestoreRef('record', firestoreRef, { serialize: vuexfireSerialize });
+    }),
+    unbind: firestoreAction(({ unbindFirestoreRef }) => {
+      return unbindFirestoreRef('record');
+    }),
+  },
+  state: {
+    record: null,
+  },
+  getters: {
+    id: (state) => {
+      if (state.record === null) return null;
+
+      return state.record.id;
+    },
+  },
+};
