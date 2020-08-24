210ed7280d6059e40a0c38da99110a3cc8cf0a80
Tom Russell <TR115251@hotmail.co.uk>
2020-08-24 15:33:01 +0100

    Add modal component and inital tests

---
+++ tests/unit/components/Page/Modal.spec.js
@@ -0,0 +1,71 @@
+import { createTestSubject } from '../../helpers';
+import Modal from '@/components/Page/Modal';
+
+describe('components/Form/Modal', () => {
+  describe('props', () => {
+    describe('cancelable', () => {
+      let prop;
+        beforeEach(() => {
+          prop = Modal.props.cancelable;   
+        });
+        it('is Boolean', () => {
+            expect(prop.type()).toBeBoolean();
+        });
+        it('is true by default', () => {
+            expect(prop.default).toBeTrue();
+        });
+      });
+    describe('buttonText', () => {
+      let prop;
+      beforeEach(() => {
+        prop = Modal.props.buttonText;   
+      });
+      it('default is \'Accept\'', () => {
+        expect(prop.default).toBe('Accept');
+      });
+      it('is a string', () => {
+        expect(prop.type()).toBeString();
+      });
+    });
+    describe('message', () => {
+      let prop;
+      beforeEach(() => {
+        prop = Modal.props.message;   
+      });
+      it('default is \'Accept\'', () => {
+        expect(prop.default).toBe('Please Confirm');
+      });
+      it('is a string', () => {
+          expect(prop.type()).toBeString();
+      });
+    });
+  });
+  describe('component instance', () => {
+    let wrapper;
+    beforeEach(()=>{
+      wrapper = createTestSubject(Modal, {
+        propsData: {
+          buttonText: 'Start Now',
+          cancelable: true,
+          message: 'Are you sure you want to start your qualifying test? The timer will begin if you do:',
+        },
+        stubs: [],
+      });
+    });
+    it('renders the component', () => {
+      expect(wrapper.exists()).toBe(true);
+    });
+    it('close modal emit', () => {
+      const emitted = wrapper.emitted();
+      expect(emitted.closed).toBeFalsy;
+      wrapper.vm.closeModal();
+      expect(emitted.closed).toBeTruthy;
+    });
+    it('confirm modal emit', () => {
+      const emitted = wrapper.emitted();
+      expect(emitted.confirmed).toBeFalsy;
+      wrapper.vm.confirmModal();
+      expect(emitted.confirmed).toBeTruthy;
+    });
+  });
+});
