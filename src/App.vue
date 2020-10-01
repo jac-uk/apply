6eb9fe9be2c175fdb6212d12f42a71e632d5f739
Tom Russell <TR115251@hotmail.co.uk>
2020-10-01 15:52:15 +0100

    wip

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,6 @@
 <template>
   <div class="page-container">
     <Header v-if="!fullPageMode" />
-    <div
-      v-if="!fullPageMode"
-      class="govuk-phase-banner govuk-!-margin-bottom-4 print-none"
-    >
-      <p class="govuk-phase-banner__content">
-        <strong class="govuk-tag govuk-phase-banner__content__tag">
-          alpha
-        </strong>
-        <span class="govuk-phase-banner__text">
-          This is a new service – your <a
-            style="font-size: 16px"
-            class="govuk-link govuk-body"
-            href="https://docs.google.com/forms/d/e/1FAIpQLSexm0qgMV0tOQTFP4QUSegOOX89VeYhWwuofV---JZTOEXGIQ/viewform"
-            target="_blank"
-          >feedback</a> will help us improve it.
-        </span>
-      </p>
-    </div>
 
     <main
       id="main-content"
--- src/components/Page/Header.vue
+++ src/components/Page/Header.vue
@@ -1,76 +1,131 @@
 <template>
-  <header class="govuk-header">
-    <div class="govuk-header__container govuk-!-margin-bottom-1  govuk-!-padding-top-3 govuk-!-padding-bottom-3 govuk-width-container">
-      <div
-        class="header-background clearfix"
-        style="display: flex;"
-      >
-        <div class="govuk-grid-column-two-thirds organisation__margin-bottom">
-          <div class="gem-c-organisation-logo brand--ministry-of-justice">
-            <a href="https://judicialappointments.gov.uk/">
-              <img
-                src="@/assets/jac-logo.svg"
-                alt="Judicial Appointments Commission"
-                width="197"
-                height="66"
-                style="text-align: center"
-              >
-            </a>
-          </div>
-        </div>
-        <div class="govuk-grid-column-one-third organisation__margin-bottom">
-          <div 
-            v-if="isSignedIn"
-            class="moj-button-menu float-right"
-          >
-            <button
-              ref="dropDownRef"
-              class="govuk-button drop-down-button moj-button-menu__toggle-button govuk-button--secondary moj-button-menu__toggle-button--secondary govuk-!-margin-bottom-0"
-              type="button"
-              aria-haspopup="true"
-              :aria-expanded="dropDownExpanded.toString()"
-              @click="toggleExpand"
-            >
-              Account
-            </button>
-            <div
-              class="moj-button-menu__wrapper float-left"
-              role="menu"
-            >
-              <button
-                class="drop-down-button govuk-!-margin-right-1"
-                style="pointer-events: none; word-break: break-word;"
-              >
-                Signed in as {{ userName }}
-              </button>
-              <button
-                class="govuk-button govuk-button--secondary drop-down-button govuk-!-margin-right-1"
-                @click="signOut"
-              >
-                Sign Out
-              </button>
+  <div>
+    <header class="govuk-header govuk-!-padding-2">
+      <div class="govuk-header__container govuk-!-margin-bottom-1 govuk-width-container">
+        <div
+          class="header-background clearfix"
+          style="display: flex;"
+        >
+          <div class="govuk-grid-column-two-thirds organisation__margin-bottom">
+            <div class="gem-c-organisation-logo brand--ministry-of-justice">
+              <a href="https://judicialappointments.gov.uk/">
+                <img
+                  src="@/assets/jac-logo.svg"
+                  alt="Judicial Appointments Commission"
+                  width="197"
+                  height="66"
+                  style="text-align: center"
+                >
+              </a>
             </div>
           </div>
-          <div v-else>
+          
+          <div class="govuk-grid-column-one-third organisation__margin-bottom">
             <button
+              v-if="isSignedIn"
+              class="govuk-button govuk-button--secondary govuk-!-margin-right-1"
+              data-module="govuk-button"
+              @click="signOut"
+            >
+              Sign Out
+            </button>
+            <button
+              v-else
               :disabled="isSignInPage"
-              class="govuk-button govuk-button--secondary float-right"
+              class="govuk-button govuk-button--secondary govuk-!-margin-right-1"
+              data-module="govuk-button"
               @click="signIn"
             >
               Sign In
             </button>
+            <button
+              :disabled="isVacanciesPage"
+              class="govuk-button govuk-button--secondary"
+              data-module="govuk-button"
+              @click="gotoVacancies"
+            >
+              Vacancies
+            </button>
+            <br>
+            <span
+              v-if="isSignedIn"
+              class="app-c-topic-list__item nostyle"
+            >
+              <b>You are now signed in as {{ userName }}</b>
+            </span>
           </div>
+          
+          <!-- <div class="govuk-grid-column-one-third organisation__margin-bottom">
+            <div 
+              v-if="isSignedIn"
+              class="moj-button-menu float-right"
+            >
+              <button
+                ref="dropDownRef"
+                class="govuk-button drop-down-button moj-button-menu__toggle-button govuk-button--secondary moj-button-menu__toggle-button--secondary govuk-!-margin-bottom-0"
+                type="button"
+                aria-haspopup="true"
+                :aria-expanded="dropDownExpanded.toString()"
+                @click="toggleExpand"
+              >
+                Account
+              </button>
+              <div
+                class="moj-button-menu__wrapper float-left"
+                role="menu"
+              >
+                <button
+                  class="drop-down-button govuk-!-margin-right-1"
+                  style="pointer-events: none; word-break: break-word;"
+                >
+                  Signed in as {{ userName }}
+                </button>
+                <button
+                  class="govuk-button govuk-button--secondary drop-down-button govuk-!-margin-right-1"
+                  @click="signOut"
+                >
+                  Sign Out
+                </button>
+              </div>
+            </div>
+            <div v-else>
+              <button
+                :disabled="isSignInPage"
+                class="govuk-button govuk-button--secondary float-right"
+                @click="signIn"
+              >
+                Sign In
+              </button>
+            </div>
+          </div> -->
         </div>
       </div>
+    </header>
+    <div
+      class="govuk-phase-banner govuk-!-margin-bottom-4 print-none govuk-width-container"
+    >
+      <p class="govuk-phase-banner__content">
+        <strong class="govuk-tag govuk-phase-banner__content__tag">
+          alpha
+        </strong>
+        <span class="govuk-phase-banner__text">
+          This is a new service – your <a
+            style="font-size: 16px"
+            class="govuk-link govuk-body"
+            href="https://docs.google.com/forms/d/e/1FAIpQLSexm0qgMV0tOQTFP4QUSegOOX89VeYhWwuofV---JZTOEXGIQ/viewform"
+            target="_blank"
+          >feedback</a> will help us improve it.
+        </span>
+      </p>
     </div>
-  </header>
+  </div>
   <!-- <button
-  :disabled="isVacanciesPage"
-  class="govuk-button"
-  @click="gotoVacancies"
->
-  Vacancies
-</button> -->
+    :disabled="isVacanciesPage"
+    class="govuk-button"
+    @click="gotoVacancies"
+  >
+    Vacancies
+  </button> -->
 </template>
 <script>
 import { auth } from '@/firebase';
@@ -120,8 +175,8 @@ export default {
     // padding: 1.25em 0.625em 0 0.625em;
     // margin: 0 !important;
     // width: 99%;
-    // overflow: hidden;
-    // max-width: 100vw !important;
+    overflow: hidden;
+    // max-width: $max-width !important;
   };
   .govuk-header__container{
     border-bottom: 0 !important;
