3e279bcac0e06a7e4e0a9bd94171e459c80f2363
Lisias (Lee) Loback <lloback@users.noreply.github.com>
2020-11-20 15:17:33 +0000

    Revert "Avoid redundant navigation on router prototype (#591)"
    This reverts commit c1a46ee9425c8bc5a3274287857292ba19061692.

--- tests/journeys/page-titles.spec.js
+++ tests/journeys/page-titles.spec.js
@@ -1,56 +1,56 @@
-import router from '@/router';
-import store from '@/store';
+// @FIXME@ *error-four* Quite a different test base, couldnt manage to successfully integrate helpers
+// couldnt tell how to add necessary logic to either 
+// this file or ../helpers.js
+
+import App from '@/App';
+// import Router from 'vue-router';
+// import Vuex from 'vuex';
+import { createTestSubject } from '../unit/helpers';
 
 const routes = [
   // ['eligibility-checker', 'Eligibility Checker'],
   // ['eligibility-pass', 'Eligibility Pass'],
   // ['eligibility-fail', 'Eligibility Fail'],
-  ['vacancies', 'Vacancies'],
   ['task-list', 'Apply for a role task list'],
-  ['apply-character-information', 'Character information'],
+  ['apply-character-information', 'Declare character information'],
   ['equality-and-diversity-survey', 'Take the equality and diversity survey'],
   ['pre-application-judicial-education', 'Pre-application judicial education'],
-  ['apply-personal-details', 'Personal details'],
+  ['apply-personal-details', 'Add personal details'],
   ['assessors-details', 'Give independent assessors details'],
-  ['self-assessment-competencies', 'Self-assessment competencies'],
+  ['self-assessment-competencies', 'Upload self-assessment competencies'],
   ['review', 'Review application'],
-  ['judicial-experience', 'Judicial experience'],
-  ['post-qualification-work-experience', 'Post-qualification work experience'],
-  ['relevant-qualifications', 'Relevant qualifications'],
-  ['part-time-working-preferences', 'Part-time working preferences'],
+  ['judicial-experience', 'Add judicial experience'],
+  ['post-qualification-work-experience', 'Add post-qualification work experience'],
+  ['relevant-qualifications', 'Add relevant qualifications'],
+  ['part-time-working-preferences', 'Set part-time working preferences'],
   ['leadership-statement-of-suitability', 'Statement of suitability'],
   ['statement-of-suitability', 'Statement of suitability'],
   ['confirmation', 'Confirmation'],
 ];
 
-describe('Page titles', () => {
+xdescribe('Page titles', () => {
+  let wrapper;
+  beforeEach(() => {
+    // const localVue = createLocalVue();
+    // localVue.use(Router);
+    // localVue.use(Vuex);
 
-  const pushSpy = jest.spyOn(router, 'push');
+    // router = require('@/router').default;
+    // store = require('@/store').default;
+    window.scrollTo = () => {};
+    wrapper = createTestSubject(App, {
+      stubs: ['RouterView'],
+    });
+  });
+
   const user = {
     uid: 'abc123',
     email: 'user@judicialappointments.digital',
   };
-  
-  describe('router', () => {
-    beforeEach(() => {
-      router.push({ name: 'sign-in' });
-      store.dispatch('auth/setCurrentUser', user);
-      pushSpy.mockClear();
-    });
-    
-    it('wont push to same route', () => {
-      router.push({ name: 'vacancies' });
-      expect(document.title).toContain('Vacancies');
-      expect(pushSpy).toHaveBeenCalledTimes(1);
-      router.push({ name: 'vacancies' });
-      expect(document.title).toContain('Vacancies');
-    });    
-
-  });
 
   describe('sign in', () => {
     beforeEach(() => {
-      router.push({ name: 'sign-in' });
+      wrapper.vm.$router.push({ name: 'sign-in' });
     });
 
     it('contains Sign In', () => {
@@ -64,14 +64,14 @@ describe('Page titles', () => {
 
   describe.each(routes)('%s', (routeName, routeTitle) => {
     beforeEach(() => {
-      store.dispatch('auth/setCurrentUser', user);
-      router.push({ name: routeName, params: { id: 123 } });
+      wrapper.vm.$store.dispatch('auth/setCurrentUser', user);
+      wrapper.vm.$router.push({ name: routeName, params: { id: 123 } });
     });
 
     it(`contains ${routeTitle}`, () => {
       expect(document.title).toContain(routeTitle);
     });
-    
+
     it('contains Judicial Appointments Commission', () => {
       expect(document.title).toContain('Judicial Appointments Commission');
     });
