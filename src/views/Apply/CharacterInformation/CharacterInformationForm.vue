639cc510ef89dea473f9afa927cb03dfce295e35
Maria Brookes <maria_brookes@yahoo.co.uk>
2020-12-01 17:01:52 +0000

    #565 Updated criminal offences page

---
+++ src/views/Apply/CharacterInformation/CharacterInformationForm.vue
@@ -0,0 +1,16 @@
+<script>
+import Form from '@/components/Form/Form';
+
+export default {
+  name: 'CharacterInformationForm',
+  extends: Form,
+  methods: {
+    updateProgress() {
+      this.application.progress.characterInformation =
+        this.characterInformation.criminalCautions !== undefined &&
+        this.characterInformation.criminalConvictions !== undefined;
+    },
+  },
+};
+</script>
+
