00f9a417c1157f5a5903ac3d65ffac94b90cfdde
Lisias (Lee) Loback <lisias@loback.co.uk>
2020-12-02 02:18:52 +0000

    #618 Candidates with extension can apply - tests

---
+++ tests/unit/helpers/isVacancyOpen.spec.js
@@ -0,0 +1,57 @@
+import isVacancyOpen from '@/helpers/isVacancyOpen';
+
+let today = new Date();
+let dateInTheFuture = new Date(today.setDate(today.getDate() + 7));
+today = new Date();
+let dateInThePast = new Date(today.setDate(today.getDate() - 7));
+let dateInThePastLong = new Date(today.setDate(today.getDate() - 30));
+
+// console.log('dateInTheFuture', dateInTheFuture);
+// console.log('dateInThePast', dateInThePast);
+// console.log('dateInThePastLong', dateInThePastLong);
+
+// isVacancyOpen(openDate, closeDate, extensionDate)
+
+describe('Helpers isVacancyOpen', () => {
+
+  describe('Vacancy is Open', () => {
+    it('OpenDate in the past and close date in the future', () => {
+      expect(isVacancyOpen(dateInThePastLong, dateInTheFuture, undefined)).toBe(true);
+    });
+
+    it('ExtendedDate in the future overrides closeDate', () => {
+      expect(isVacancyOpen(dateInThePastLong, dateInThePast, dateInTheFuture)).toBe(true);
+    });
+  });
+
+  describe('Vacancy is Closed', () => {
+
+    it('Open and close dates in the past', () => {
+      expect(isVacancyOpen(dateInThePastLong, dateInThePast, undefined)).toBe(false);
+    });
+
+    it('ExtendedDate in the past overrides closeDate', () => {
+      expect(isVacancyOpen(dateInThePastLong, dateInThePast, dateInThePastLong)).toBe(false);
+    });
+
+    it('no openDate', () => {
+      expect(isVacancyOpen('', dateInThePast, dateInTheFuture)).toBe(false);
+    });
+
+    it('no closeDate', () => {
+      expect(isVacancyOpen(dateInThePastLong, '', dateInTheFuture)).toBe(false);
+    });
+
+    it('null values', () => {
+      expect(isVacancyOpen(null, null, null)).toBe(false);
+    });
+
+    it('empty values', () => {
+      expect(isVacancyOpen('', '', '')).toBe(false);
+    });
+
+    it('undefined values', () => {
+      expect(isVacancyOpen(undefined, undefined, undefined)).toBe(false);
+    });
+  });
+});
