df5429b029953fea8f298870f096c6f43b75f0f1
Lisias (Lee) Loback <lisias@loback.co.uk>
2020-12-07 22:18:06 +0000

    #277 Get vacancy open and close time from database - remove the debug code

--- src/views/Apply/Apply.vue
+++ src/views/Apply/Apply.vue
@@ -43,40 +43,30 @@ export default {
       if (vacancy === null) {
         this.redirectToErrorPage();
       } else if (this.$store.getters['vacancy/getOpenDate'] > today) {
-        console.log('aplly / 1');
         this.redirectToVacancyDetails();
       } else {
-        console.log('aplly / 2');
         await this.$store.dispatch('candidate/bind');
         await this.$store.dispatch('application/bind');
 
         if (!this.$store.state.application.record) {
-          console.log('aplly / 3');
           if (vacancy.inviteOnly) {
             if (userInvitation) {
-              console.log('aplly / 4');
               await this.$store.dispatch('invitations/acceptInvitation', userInvitation.id);
             }
             else {
-              console.log('aplly / 5');
               this.redirectToErrorPage();
             }
           }
-          console.log('aplly / 6');
           await this.$store.dispatch('application/save', {
             status: 'draft',
             progress: { started: true },
           });
-          console.log('aplly / 7');
         }
-        console.log('aplly / 8');
         await this.$store.dispatch('applications/bind');
-        console.log('aplly / 9');
         this.loaded = true;
       }
     } catch (e) {
       this.loadFailed = true;
-      console.log('load failed');
       throw e;
     }
   },
