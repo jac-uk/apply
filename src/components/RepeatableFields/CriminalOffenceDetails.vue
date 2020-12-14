b48f87d9da1cd9b1fc2d4cb2eaf904fdddab64d0
Maria Brookes <maria_brookes@yahoo.co.uk>
2020-12-14 17:01:13 +0000

    #569 Refactor

--- src/components/RepeatableFields/CriminalOffenceDetails.vue
+++ src/components/RepeatableFields/CriminalOffenceDetails.vue
@@ -1,4 +1,5 @@
 <template>
+<<<<<<< Updated upstream
   <div class="govuk-!-margin-top-6">
     <TextField
       :id="`offence_title_${name}_${index}`"
@@ -11,6 +12,18 @@
       v-model="row.date"
       label="Date of offence"
       required
+=======
+  <div>
+    <TextField
+      :id="`offence_title_${index}`"
+      v-model="row.title"
+      label="Sentence, penalty or fine"
+>>>>>>> Stashed changes
+    />
+    <DateInput
+      :id="`offence_date_${index}`"
+      v-model="row.date"
+      label="Date of offence"
     />
     <TextareaInput
       :id="`offence_details_${name}_${index}`"
@@ -45,21 +58,27 @@ export default {
       required: true,
       type: Number,
     },
+<<<<<<< Updated upstream
     label: {
       required: true,
       type: String,
       default: '',
     },
+=======
+>>>>>>> Stashed changes
     hint: {
       required: false,
       type: String,
       default: '',
     },
+<<<<<<< Updated upstream
     name: {
       required: false,
       type: String,
       default: 'criminal-offences',
     },
+=======
+>>>>>>> Stashed changes
   },
 };
 </script>
