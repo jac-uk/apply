9e62b6af88490ec4ea06663ba8f85420ea76e604
Tom Russell <TR115251@hotmail.co.uk>
2021-01-11 17:03:27 +0000

    additions to not found page

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -2,6 +2,14 @@
   <div class="govuk-heading-xl govuk-body">
     Oops, the page you requested does not exist or you no longer have access to it.
     <br>
+    <br>
+    If you can’t find what you are looking for please
+    <a
+      href="https://judicialappointments.gov.uk/about-the-jac/contact/"
+      class="govuk-link govuk-body"
+    >
+      contact us.
+    </a>
     <router-link
       to="/"
       class="govuk-link govuk-body"
