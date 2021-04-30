187d251d5d41130b07d8b7d155c5989aabc3a698
HalcyonJAC <halcyon@judicialappointments.digital>
2021-04-30 17:44:03 +0100

    Review - Moved Judicial Experience to its own component

--- src/views/Apply/FinalCheck/Review.vue
+++ src/views/Apply/FinalCheck/Review.vue
@@ -459,89 +459,10 @@
               </RouterLink>
             </div>
 
-            <dl
-              class="govuk-summary-list govuk-!-margin-bottom-8"
-            >
-              <div
-                class="govuk-summary-list__row"
-              >
-                <dt
-                  v-if="vacancy.previousJudicialExperienceApply"
-                  class="govuk-summary-list__key"
-                >
-                  Are you a fee-paid or salaried judge?
-                </dt>
-                <dd class="govuk-summary-list__value">
-                  {{ application.feePaidOrSalariedJudge | toYesNo }}
-                </dd>
-              </div>
-
-              <div
-                v-if="application.feePaidOrSalariedJudge === true"
-                class="govuk-summary-list__row"
-              >
-                <dt class="govuk-summary-list__key">
-                  {{ `Have you sat for at least ${vacancy.pjeDays || 30 } days?` }}
-                </dt>
-                <dd class="govuk-summary-list__value">
-                  <p class="govuk-body">
-                    {{ application.feePaidOrSalariedSatForThirtyDays | toYesNo }}
-                  </p>
-                  <p
-                    v-if="application.feePaidOrSalariedSittingDaysDetails"
-                    class="govuk-body"
-                  >
-                    {{ application.feePaidOrSalariedSittingDaysDetails }}
-                  </p>
-                </dd>
-              </div>
-
-              <div
-                v-if="application.feePaidOrSalariedSatForThirtyDays == false || application.feePaidOrSalariedJudge == false"
-                class="govuk-summary-list__row"
-              >
-                <dt class="govuk-summary-list__key">
-                  Have you declared an appointment or appointments in a quasi-judicial body in this application?
-                </dt>
-                <dd class="govuk-summary-list__value">
-                  {{ application.declaredAppointmentInQuasiJudicialBody | toYesNo }}
-                </dd>
-              </div>
-
-              <div
-                v-if="application.declaredAppointmentInQuasiJudicialBody === true"
-                class="govuk-summary-list__row"
-              >
-                <dt class="govuk-summary-list__key">
-                  {{ `Have you sat for at least ${ vacancy.pjeDays || 30 } in one or all of these appointments?` }}
-                </dt>
-                <dd class="govuk-summary-list__value">
-                  <p class="govuk-body">
-                    {{ application.quasiJudicialSatForThirtyDays | toYesNo }}
-                  </p>
-                  <p
-                    v-if="application.quasiJudicialSittingDaysDetails"
-                    class="govuk-body"
-                  >
-                    {{ application.quasiJudicialSittingDaysDetails }}
-                  </p>
-                </dd>
-              </div>
-
-              <div
-                v-if="application.declaredAppointmentInQuasiJudicialBody == false ||
-                  application.quasiJudicialSatForThirtyDays == false"
-                class="govuk-summary-list__row"
-              >
-                <dt class="govuk-summary-list__key">
-                  details of how you have acquired the necessary
-                  skills for this role in some other significant way
-                </dt>
-                <dd class="govuk-summary-list__value">
-                  {{ application.skillsAquisitionDetails }}
-                </dd>
-              </div>
-            </dl>
+            <JudicialExperience
+              :application="application"
+              :vacancy="vacancy"
+            />
           </div>
 
           <div
@@ -1039,6 +960,7 @@ import Qualifications from './Qualifications.vue';
 import Schedule2 from './Schedule2.vue';
 import Memberships from './Memberships.vue';
 import PostQualificationExperience from './PostQualificationExperience.vue';
+import JudicialExperience from './JudicialExperience.vue';
 
 export default {
   components: {
@@ -1060,6 +982,7 @@ export default {
     Schedule2,
     Memberships,
     PostQualificationExperience,
+    JudicialExperience,
   },
   mixins: [ApplyMixIn],
   data() {
