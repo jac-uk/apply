bb398031bd7457f7b2cbf4f365ebe7cb4b8c03e0
HalcyonJAC <halcyon@judicialappointments.digital>
2021-04-30 17:47:22 +0100

    Review - Moved Gaps in Employment section to its own component

--- src/views/Apply/FinalCheck/Review.vue
+++ src/views/Apply/FinalCheck/Review.vue
@@ -484,67 +484,9 @@
             </RouterLink>
           </div>
 
-          <dl
-            v-for="item in application.employmentGaps"
-            :key="item.name"
-            class="govuk-summary-list govuk-!-margin-bottom-8"
-          >
-            <div class="govuk-summary-list__row">
-              <dt class="govuk-summary-list__key">
-                Date of gap
-              </dt>
-              <dd class="govuk-summary-list__value">
-                <ul
-                  v-if="item.startDate"
-                  class="govuk-list"
-                >
-                  <li v-if="item.endDate">
-                    {{ item.startDate | formatDate }} to {{ item.endDate | formatDate }}
-                  </li>
-                  <li v-else>
-                    {{ item.startDate | formatDate }} — current
-                  </li>
-                </ul>
-              </dd>
-            </div>
-
-            <div class="govuk-summary-list__row">
-              <dt class="govuk-summary-list__key">
-                Details
-              </dt>
-              <dd class="govuk-summary-list__value">
-                <ul class="govuk-list">
-                  <li>{{ item.details }}</li>
-                </ul>
-              </dd>
-            </div>
-
-            <div
-              v-if="isLegal"
-              class="govuk-summary-list__row"
-            >
-              <dt class="govuk-summary-list__key">
-                Law-related tasks
-              </dt>
-              <dd class="govuk-summary-list__value">
-                <ul class="govuk-list">
-                  <li
-                    v-for="task in item.tasks"
-                    :key="task.name"
-                  >
-                    <p
-                      v-if="task == 'other'"
-                      class="govuk-body govuk-!-margin-bottom-0"
-                    >
-                      <span class="govuk-caption-m">{{ task | lookup }}</span>
-                      {{ item.otherTasks }}
-                    </p>
-                    <span v-else>{{ task | lookup }}</span>
-                  </li>
-                </ul>
-              </dd>
-            </div>
-          </dl>
+          <GapsInEmployment
+            :application="application"
+          />
 
           <div class="govuk-!-margin-top-9">
             <h2
@@ -961,6 +903,7 @@ import Schedule2 from './Schedule2.vue';
 import Memberships from './Memberships.vue';
 import PostQualificationExperience from './PostQualificationExperience.vue';
 import JudicialExperience from './JudicialExperience.vue';
+import GapsInEmployment from './GapsInEmployment';
 
 export default {
   components: {
@@ -983,6 +926,7 @@ export default {
     Memberships,
     PostQualificationExperience,
     JudicialExperience,
+    GapsInEmployment,
   },
   mixins: [ApplyMixIn],
   data() {
