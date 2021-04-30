bf0e1516cf5ce1c029f65de5645a9dae18949854
HalcyonJAC <halcyon@judicialappointments.digital>
2021-04-30 17:43:53 +0100

    Review - Moved Post-Qualification Experience to its own component

---
+++ src/views/Apply/FinalCheck/PostQualificationExperience.vue
@@ -0,0 +1,84 @@
+<template>
+  <div>
+    <dl
+      v-for="item in application.experience"
+      :key="item.name"
+      class="govuk-summary-list govuk-!-margin-bottom-8"
+    >
+      <div class="govuk-summary-list__row">
+        <dt class="govuk-summary-list__key">
+          Job title
+        </dt>
+        <dd class="govuk-summary-list__value">
+          <ul class="govuk-list">
+            <li>{{ item.jobTitle }}</li>
+          </ul>
+        </dd>
+      </div>
+
+      <div class="govuk-summary-list__row">
+        <dt class="govuk-summary-list__key">
+          Organisation or business
+        </dt>
+        <dd class="govuk-summary-list__value">
+          <ul class="govuk-list">
+            <li>{{ item.orgBusinessName }}</li>
+          </ul>
+        </dd>
+      </div>
+
+      <div class="govuk-summary-list__row">
+        <dt class="govuk-summary-list__key">
+          Dates worked
+        </dt>
+        <dd class="govuk-summary-list__value">
+          <ul
+            v-if="item.startDate"
+            class="govuk-list"
+          >
+            <li v-if="item.endDate">
+              {{ item.startDate | formatDate }} to {{ item.endDate | formatDate }}
+            </li>
+            <li v-else>
+              {{ item.startDate | formatDate }} — current
+            </li>
+          </ul>
+        </dd>
+      </div>
+
+      <div class="govuk-summary-list__row">
+        <dt class="govuk-summary-list__key">
+          Law-related tasks
+        </dt>
+        <dd class="govuk-summary-list__value">
+          <ul class="govuk-list">
+            <li
+              v-for="task in item.tasks"
+              :key="task.name"
+            >
+              <p
+                v-if="task == 'other'"
+                class="govuk-body govuk-!-margin-bottom-0"
+              >
+                <span class="govuk-caption-m">{{ task | lookup }}</span>
+                {{ item.otherTasks }}
+              </p>
+              <span v-else>{{ task | lookup }}</span>
+            </li>
+          </ul>
+        </dd>
+      </div>
+    </dl>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    application: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
--- src/views/Apply/FinalCheck/Review.vue
+++ src/views/Apply/FinalCheck/Review.vue
@@ -436,75 +436,9 @@
               </RouterLink>
             </div>
 
-            <dl
-              v-for="item in application.experience"
-              :key="item.name"
-              class="govuk-summary-list govuk-!-margin-bottom-8"
-            >
-              <div class="govuk-summary-list__row">
-                <dt class="govuk-summary-list__key">
-                  Job title
-                </dt>
-                <dd class="govuk-summary-list__value">
-                  <ul class="govuk-list">
-                    <li>{{ item.jobTitle }}</li>
-                  </ul>
-                </dd>
-              </div>
-
-              <div class="govuk-summary-list__row">
-                <dt class="govuk-summary-list__key">
-                  Organisation or business
-                </dt>
-                <dd class="govuk-summary-list__value">
-                  <ul class="govuk-list">
-                    <li>{{ item.orgBusinessName }}</li>
-                  </ul>
-                </dd>
-              </div>
-
-              <div class="govuk-summary-list__row">
-                <dt class="govuk-summary-list__key">
-                  Dates worked
-                </dt>
-                <dd class="govuk-summary-list__value">
-                  <ul
-                    v-if="item.startDate"
-                    class="govuk-list"
-                  >
-                    <li v-if="item.endDate">
-                      {{ item.startDate | formatDate }} to {{ item.endDate | formatDate }}
-                    </li>
-                    <li v-else>
-                      {{ item.startDate | formatDate }} — current
-                    </li>
-                  </ul>
-                </dd>
-              </div>
-
-              <div class="govuk-summary-list__row">
-                <dt class="govuk-summary-list__key">
-                  Law-related tasks
-                </dt>
-                <dd class="govuk-summary-list__value">
-                  <ul class="govuk-list">
-                    <li
-                      v-for="task in item.tasks"
-                      :key="task.name"
-                    >
-                      <p
-                        v-if="task == 'other'"
-                        class="govuk-body govuk-!-margin-bottom-0"
-                      >
-                        <span class="govuk-caption-m">{{ task | lookup }}</span>
-                        {{ item.otherTasks }}
-                      </p>
-                      <span v-else>{{ task | lookup }}</span>
-                    </li>
-                  </ul>
-                </dd>
-              </div>
-            </dl>
+            <PostQualificationExperience
+              :application="application"
+            />
           </div>
 
           <div v-if="isLegal && vacancy.previousJudicialExperienceApply">
@@ -1104,6 +1038,7 @@ import AdditionalWorkingPreferences from './AdditionalWorkingPreferences';
 import Qualifications from './Qualifications.vue';
 import Schedule2 from './Schedule2.vue';
 import Memberships from './Memberships.vue';
+import PostQualificationExperience from './PostQualificationExperience.vue';
 
 export default {
   components: {
@@ -1124,6 +1059,7 @@ export default {
     Qualifications,
     Schedule2,
     Memberships,
+    PostQualificationExperience,
   },
   mixins: [ApplyMixIn],
   data() {
