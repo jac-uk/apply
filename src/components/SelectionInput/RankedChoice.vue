57fc698724aefde8b8d0de9783084e0293058c93
Tom Russell <TR115251@hotmail.co.uk>
2021-05-05 16:59:05 +0100

    wip

--- src/components/SelectionInput/RankedChoice.vue
+++ src/components/SelectionInput/RankedChoice.vue
@@ -35,13 +35,6 @@
         </option>
       </select>      
     </div>
-    selected:{{ selected }}
-    <br>
-    ranking: {{ ranking }}
-    <br>
-    <!-- selectable: {{ selectable }} -->
-    <br>
-    <!-- answers: {{ answers }} -->
   </div>
 </template>
 
@@ -85,8 +78,6 @@ export default {
   methods: {
     update(event, answer) {
       const sortRanking = {};
-      // if (this.selected.length) {
-      //   // if (this.selected.length < Object.keys(this.ranking).length) {
       if (this.selected.length > 1) {
         this.selected.forEach((selection, i) => {
           sortRanking[selection] = i + 1;
@@ -95,63 +86,32 @@ export default {
         this.ranking = {
           [`${this.selected[0]}`]: 1,
         };
-        // choice swap
-        const targetValue = parseInt(event.target.value);
-        console.log(targetValue);
-        if (targetValue) {
-          
-          const indexOfDuplicate = Object.values(sortRanking).findIndex((element) => element === targetValue);
-          const duplicateItem = Object.keys(sortRanking)[indexOfDuplicate];
-
-          sortRanking[duplicateItem] = sortRanking[answer];
-          sortRanking[answer] = targetValue;
-
-          this.ranking = sortRanking;
-          const rankedOrder = [];
-
-          for (const [item, rank] of Object.entries(this.ranking)) {
-            // console.log(item, rank);
-            rankedOrder[parseInt(rank) - 1] = item;
-          }
-
-          this.selected = rankedOrder;
-
-          this.$emit('input', this.selected);
-        }
       }
+
+      // choice swap
+      const targetValue = parseInt(event.target.value);
+      if (!isNaN(targetValue)) {
+          
+        const indexOfDuplicate = Object.values(sortRanking).findIndex((element) => element === targetValue);
+        const duplicateItem = Object.keys(sortRanking)[indexOfDuplicate];
+
+        sortRanking[duplicateItem] = sortRanking[answer];
+        sortRanking[answer] = targetValue;
+
+        this.ranking = sortRanking;
+        const rankedOrder = [];
+
+        for (const [item, rank] of Object.entries(this.ranking)) {
+          // console.log(item, rank);
+          rankedOrder[parseInt(rank) - 1] = item;
+        }
+
+        this.selected = rankedOrder;
+
+        this.$emit('input', this.selected);
+      }
+
     },
-    // update() {
-    //   const rankedSelection = [];
-    //   const cleanedRanking = {};
-
-    //   if (this.selected.length > 1) {
-      
-    //     this.selected.forEach((selection) => {
-      
-    //       if (!this.ranking[selection]) { 
-    //         this.ranking[selection] = this.selected.length; 
-    //       }
-
-    //       rankedSelection.push({ answer: selection, rank: this.ranking[selection] });
-    //       cleanedRanking[selection] = this.ranking[selection];
-    //     });
-
-    //     this.ranking = cleanedRanking;
-    //     this.selected = rankedSelection.sort(( item1, item2 ) => {
-    //       if (item1.rank < item2.rank) {
-    //         return -1;
-    //       } else if (item1.rank > item2.rank) {
-    //         return 1;
-    //       } else {
-    //         return 0;
-    //       }
-    //     }).map((item) => {
-    //       return item.answer;
-    //     });
-    //   }
-
-    //   this.$emit('input', this.selected);
-    // },
   },  
 };
 
