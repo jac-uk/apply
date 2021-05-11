d5fc737f5ab689655d90dad2c1483094e7973844
Tom Gillespy <halcyon@judicialappointments.digital>
2021-05-11 13:29:58 +0100

    Reset PW component with proper validation

---
+++ src/views/ConfirmResetPassword.vue
@@ -0,0 +1,102 @@
+<template>
+  <div class="govuk-grid-row">
+    <div class="govuk-grid-column-full">
+      <form
+        ref="formRef"
+        @submit.prevent="onSubmit"
+      >
+        <div class="govuk-grid-column-two-thirds">
+          <h1 class="gov-heading-xl">
+            Reset Password
+          </h1>
+
+          <div
+            v-if="resetSuccessful"
+            class="govuk-panel govuk-panel--confirmation"
+          >
+            <h1 class="govuk-panel__title">
+              Your password has been changed
+            </h1>
+            <h2 class="govuk-panel__body govuk-!-font-size-27 govuk-!-margin-top-7">
+              You may now <RouterLink
+                class="govuk-link"
+                data-module="govuk-button"
+                :to="{ name: 'sign-in' }"
+              >
+                sign in
+              </RouterLink> with your new password.
+            </h2>
+          </div>
+          <div v-if="!resetSuccessful">
+
+            <Password
+              id="password"
+              v-model="formData.password"
+              label="Password"
+              hint="For security reasons it should be 8 or more characters long, contain a mix of upper- and lower-case letters, at least one digit and special character (like £, #, @, !, %, -, &, *)."
+              type="new-password"
+              :min-length="8"
+              required
+            />
+
+            <button
+              type="submit"
+              class="govuk-button"
+            >
+              Confirm Password Reset
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+import { auth } from '@/firebase';
+import Password from '../components/Form/Password';
+import Form from '../components/Form/Form';
+
+export default {
+  name: 'ConfirmResetPassword',
+  components: { Password },
+  extends: Form,
+  data() {
+    return {
+      resetSuccessful: false,
+      formData: {},
+      errors: [],
+      resetCode: null,
+    };
+  },
+  mounted() {
+    this.getResetCodeFromUrl();
+  },
+  methods: {
+    async onSubmit() {
+      await this.validate();
+      if (this.isValid()) {
+        try {
+          await auth().confirmPasswordReset(this.resetCode, this.formData.password);
+          this.resetSuccessful = true;
+        } catch (error) {
+          this.errors.push({ message: error.message });
+          this.scrollToTop();
+        }
+      }
+    },
+    getResetCodeFromUrl() {
+      const params = new URLSearchParams(window.location.search);
+      if (!params.has('oobCode')) {
+        this.$router.push({ name: 'not-found' });
+        return;
+      }
+      this.resetCode = params.get('oobCode');
+    },
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
