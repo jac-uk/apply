860008133e06e3392632d98c5cd097f4753899e0
Tom Russell <TR115251@hotmail.co.uk>
2021-06-29 16:08:18 +0100

    merge

--- src/components/Form/FormField.vue
+++ src/components/Form/FormField.vue
@@ -134,6 +134,7 @@ export default {
           const words = this.value
             .split(/[^a-z'-]/i) //split into array at every occurance of a character which is NOT: a-z or ' or -
             .filter(item => item != '') // remove any empty items from array
+            .filter(item => item != '\'') // remove any items which are just a apostrophe
             .filter(item => item != '-') // remove any items which are just a hyphen
             .map((item, i) => { // with the above array 
               if (i, item.replace(/[^-]/g, '').length >= 4) { // find any items containing more than or equal to 4 hyphens (4 allows for a trailing hyphen which is not counted in next set)
