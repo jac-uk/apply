e859b19e05910641af73c29294718a3fdae78b80
HalcyonJAC <79906532+HalcyonJAC@users.noreply.github.com>
2021-07-13 09:08:55 +0100

    #371 Breadcrumb component (#789)
    * added breadcrumb component
    
    * Updated breadcrumb component to match gov.uk design system
    
    * Added option to override route for breadcrumb items

--- src/router.js
+++ src/router.js
@@ -277,6 +277,7 @@ const router = new Router({
           meta: {
             requiresAuth: true,
             title: 'Character information',
+            breadcrumbRoute: 'character-information-review',
           },
           component: MainCharacterInformation,
           children: [
