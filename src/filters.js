3e4b598f548fabb77b26cf97c71ff7367ce1e13d
Tom Russell <TR115251@hotmail.co.uk>
2021-07-19 14:25:53 +0100

    remove unneeded changes

--- src/filters.js
+++ src/filters.js
@@ -3,7 +3,7 @@ import { QUALIFYING_TEST } from '@/helpers/constants';
 const capitalize = (value) => {
   if (!value) return '';
   value = value.toString();
-  return value ? value.charAt(0).toUpperCase() + value.slice(1) : '';
+  return value.charAt(0).toUpperCase() + value.slice(1);
 };
 
 const hyphenize = (value) => {
