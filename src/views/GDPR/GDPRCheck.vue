e7e0dd65124b064d7b2b19650300818249a09cda
Tom Russell <TR115251@hotmail.co.uk>
2022-04-05 17:25:23 +0100

    wording update

--- src/views/GDPR/GDPRCheck.vue
+++ src/views/GDPR/GDPRCheck.vue
@@ -4,23 +4,40 @@
       ref="formRef"
       @submit.prevent="save"
     >
-      <h1 class="govuk-heading-xl">
-        GDPR Statement
+      <h1 class="govuk-heading-xl govuk-!-margin-bottom-2">
+        Sharing information
       </h1>
+
+      <p
+        class="govuk-body"
+      >
+        We sometimes need to share the personal information we process with the individual themselves and also with other organizations. Where this is necessary we will comply with all aspects of the data protection laws. <br>
+        <br>
+        The organizations we share some aspects of your personal information with may include: <br>
+        <br>
+        - The judiciary (for statutory consultation) <br>
+        - Independent assessors (the individuals nominated by you) <br>
+        - HMRC (for tax and financial information) <br>
+        - ACRO Criminal Records Office (for criminal record checks) <br>
+        - Professional bodies (including any relevant regulatory bodies) (for conduct verification) <br>
+        - Judicial Office, HMCTS, the Ministry of Justice and Judicial College (for purposes relating to the appointment of judges) <br>
+        - Welsh Government (for devolved appointments) <br>
+        - Judicial Appointments and Conduct Ombudsman (only if you make a complaint to them)<br>
+      </p>
 
       <Checkbox
         id="consent"
         v-model="agreedToGDPR"
         required
       >
-        [GDPR AGREEMENT STATEMENT]
+        Please confirm you are content that we may share your data in this way
       </Checkbox>
 
       <button
         class="govuk-button info-btn--eligibility-check--continue"
         :disabled="!agreedToGDPR"
       >
-        Continue
+        Agree and continue
       </button>
     </form>
   </div>
