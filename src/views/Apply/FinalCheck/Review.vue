841872947e2d0d856ce529d79a3cbb4b3da089fa
HalcyonJAC <79906532+HalcyonJAC@users.noreply.github.com>
2022-10-13 10:49:06 +0100

    added uploaded self assessment document in review (#920)

--- src/views/Apply/FinalCheck/Review.vue
+++ src/views/Apply/FinalCheck/Review.vue
@@ -619,6 +619,36 @@
             </dl>
           </div>
 
+          <div v-if="applicationParts.selfAssessmentCompetencies">
+            <div class="govuk-!-margin-top-9">
+              <h2
+                class="govuk-heading-l"
+                style="display:inline-block;"
+              >
+                Self assessment with competencies
+              </h2>
+              <RouterLink
+                v-if="canEdit && currentApplicationParts.selfAssessmentCompetencies"
+                class="govuk-link govuk-body-m change-link"
+                style="display:inline-block;"
+                :to="{name: 'self-assessment-competencies'}"
+              >
+                Change
+              </RouterLink>
+            </div>
+            <dl class="govuk-summary-list">
+              <div v-if="application.uploadedSelfAssessment">
+                <DownloadLink
+                  :file-name="application.uploadedSelfAssessment"
+                  :exercise-id="vacancy.id"
+                  :user-id="application.userId"
+                  :title="application.uploadedSelfAssessment"
+                />
+              </div>
+              <span v-else>Not yet received</span>
+            </dl>
+          </div>
+
           <div v-if="applicationParts.leadershipJudgeDetails">
             <div class="govuk-!-margin-top-9">
               <h2
@@ -941,6 +971,7 @@ import Memberships from './Memberships.vue';
 import PostQualificationExperience from './PostQualificationExperience.vue';
 import JudicialExperience from './JudicialExperience.vue';
 import GapsInEmployment from './GapsInEmployment';
+import DownloadLink from '@/components/DownloadLink';
 
 export default {
   components: {
@@ -965,6 +996,7 @@ export default {
     PostQualificationExperience,
     JudicialExperience,
     GapsInEmployment,
+    DownloadLink,
   },
   mixins: [ApplyMixIn],
   data() {
