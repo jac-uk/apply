086e273b93c84db083edc0f674ef62d83a524469
warrensearle <warren@precise-minds.co.uk>
2023-04-11 10:10:00 +0100

    Hotfix/1.64.2 (#994)
    * Admin#1914 Hardcode Exercise 0149 - Exclude Welsh Applications (#991)
    
    * update version to 1.64.1
    
    * #993 Remove mandatory field against NINO
    
    * Bump to version 1.64.2
    
    ---------
    
    Co-authored-by: HalcyonJAC <79906532+HalcyonJAC@users.noreply.github.com>
    Co-authored-by: halcyonjac <halcyon@judicialappointments.digital>

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -97,7 +97,6 @@
             label="National Insurance number"
             hint="It’s on your National Insurance card, payslip or P60. For example, ‘QQ 12 34 56 C’."
             type="nino"
-            required
           />
 
           <button
