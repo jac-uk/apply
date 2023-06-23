614a2f48e636473933ab13979d8146c45d29b6ca
drieJAC <jac.drie@digitalteam.uk>
2023-06-23 11:41:03 +0100

    #1021 Barrister - Pupilage Exemption Manage Upload (#1022)
    * Updated welsh text ofr change in question.
    Added and/or for file uploads for the scenario where pupillage is set to no.
    
    * Fixed missing array check for qualifications
    
    * Updated computed property so its more rigorous
    
    * Upgraded jac-kit version
    
    * Added welsh translations for pupillage text

--- src/views/Apply/QualificationsAndExperience/RelevantQualifications.vue
+++ src/views/Apply/QualificationsAndExperience/RelevantQualifications.vue
@@ -87,6 +87,36 @@
           v-model="formData.qualifications"
           :component="repeatableFields.Qualification"
         />
+
+        <template v-if="notCompletedPupillage">
+          <h2>As you did not complete pupillage, please provide a copy of your exemption and or practicing certificate</h2>
+
+          <FileUpload
+            id="exemption-certificate-upload"
+            ref="exemption-certificate"
+            v-model="formData.uploadedExemptionCertificate"
+            name="exemption-certificate"
+            :path="uploadPath"
+            label="Exemption certificate"
+            required
+          />
+
+          <FileUpload
+            id="practicing-certificate-upload"
+            ref="practicing-certificate"
+            v-model="formData.uploadedPracticingCertificate"
+            name="practicing-certificate"
+            :path="uploadPath"
+            label="Practicing certificate"
+            required
+          />
+
+          <FormFieldError
+            id="practicing-certificate-upload"
+            :error-message="errorMessage"
+          />
+        </template>
+
         <button
           :disabled="!canSave(formId)"
           class="govuk-button info-btn--relevant-qualifications--save-and-continue"
@@ -109,6 +139,9 @@ import RadioGroup from '@/components/Form/RadioGroup';
 import TextareaInput from '@/components/Form/TextareaInput';
 import BackLink from '@/components/BackLink';
 import * as filters from '@/filters';
+import FileUpload from '@/components/Form/FileUpload';
+import FormFieldError from '@/components/Form/FormFieldError';
+import _has from 'lodash/has';
 
 export default {
   name: 'RelevantQualifications',
@@ -119,6 +152,8 @@ export default {
     RadioGroup,
     TextareaInput,
     BackLink,
+    FileUpload,
+    FormFieldError,
   },
   extends: Form,
   mixins: [ApplyMixIn],
@@ -129,6 +164,8 @@ export default {
       experienceUnderSchedule2D: null,
       applyingUnderSchedule2Three: null,
       experienceUnderSchedule2Three: null,
+      uploadedExemptionCertificate: null,
+      uploadedPracticingCertificate: null,
       progress: {},
     };
     const data = this.$store.getters['application/data'](defaults);
@@ -139,11 +176,37 @@ export default {
       repeatableFields: {
         Qualification,
       },
+      errorMessage: '',
     };
   },
   computed: {
     appliedSchedule() {
       return filters.lookup(this.vacancy.appliedSchedule);
+    },
+    notCompletedPupillage() {
+      if (_has(this.formData, 'qualifications') && Array.isArray(this.formData.qualifications)) {
+        const matches = this.formData.qualifications.filter(qualification => {
+          return qualification.type === 'barrister'
+            && 'completedPupillage' in qualification
+            && qualification.completedPupillage === false;
+        });
+        return matches.length > 0;
+      }
+      return null;
+    },
+  },
+  methods: {
+    validate() {
+      if (this.notCompletedPupillage) {
+        if (this.formData.uploadedExemptionCertificate === null && this.formData.uploadedPracticingCertificate === null) {
+          this.$refs['practicing-certificate'].setError('Please provide a copy of your practicing and/or exemption certificate');
+          this.$refs['exemption-certificate'].setError('Please provide a copy of your exemption and/or practicing certificate');
+        }
+        else {
+          this.$refs['practicing-certificate'].setError('');
+          this.$refs['exemption-certificate'].setError('');
+        }
+      }
     },
   },
 };
