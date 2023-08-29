a250e5aa5c2a9db312c48c292cf3fdb610bd1f6a
halcyonjac <halcyon@judicialappointments.digital>
2023-08-30 00:00:14 +0100

    update main.js

--- src/main.js
+++ src/main.js
@@ -1,44 +1,14 @@
-import Vue from 'vue';
+import { createApp } from 'vue';
 import App from '@/App';
 import router from '@/router';
 import store from '@/store';
 import * as filters from '@/filters';
 import { auth } from '@/firebase';
 import * as Sentry from '@sentry/vue';
-import { BrowserTracing } from '@sentry/tracing';
 import VueGtag from 'vue-gtag';
 import browserDetect from 'vue-browser-detect-plugin';
 import { VueReCaptcha } from 'vue-recaptcha-v3';
 import VueDOMPurifyHTML from 'vue-dompurify-html';
-
-if (process.env.NODE_ENV !== 'development') {
-  Sentry.init({
-    Vue,
-    dsn: 'https://2366ef9baa1a49bb8aa29c5262757de9@sentry.io/1499367',
-    environment: store.getters.appEnvironment.toLowerCase(),
-    release: process.env.PACKAGE_VERSION,
-    integrations: [
-      new BrowserTracing({
-        routingInstrumentation: Sentry.vueRouterInstrumentation(router),
-      }),
-    ],
-  });
-
-  Vue.use(VueGtag, {
-    config: { id: 'UA-153516887-1' },
-  }, router);
-}
-
-Vue.config.productionTip = false;
-
-Vue.use(browserDetect);
-Vue.use(VueDOMPurifyHTML);
-
-// Register global filters
-Object.keys(filters)
-  .forEach((filterName) => {
-    Vue.filter(filterName, filters[filterName]);
-  });
 
 let vueInstance = false;
 auth.onAuthStateChanged( (user) => {
@@ -64,18 +34,46 @@ auth.onAuthStateChanged( (user) => {
       // router.push('applications');
     }
   }
+
+  // Create the Vue instance, but only once
   if (!vueInstance) {
-    vueInstance = new Vue({
-      el: '#app',
-      render: h => h(App),
-      router,
-      store,
-    });
+
+    // Root instance
+    vueInstance = createApp(App)
+      .use(router)
+      .use(store)
+      .use(browserDetect)
+      .use(VueDOMPurifyHTML);
+
+    // Bind global filters before mounting
+    vueInstance.config.globalProperties.$filters = filters;
+
+    // Initialise Sentry
+    if (process.env.NODE_ENV !== 'development') {
+      Sentry.init({
+        app: vueInstance,
+        dsn: 'https://2366ef9baa1a49bb8aa29c5262757de9@sentry.io/1499367',
+        environment: store.getters.appEnvironment.toLowerCase(),
+        release: process.env.PACKAGE_VERSION,
+        integrations: [
+          new Sentry.BrowserTracing({
+            routingInstrumentation: Sentry.vueRouterInstrumentation(router),
+          }),
+        ],
+      });
+
+      vueInstance.use(VueGtag, {
+        config: { id: 'UA-153516887-1' },
+      }, router);
+    }
+
+    if (process.env.VUE_APP_RECAPTCHA_TOKEN) {
+      vueInstance.use(VueReCaptcha, {
+        siteKey: process.env.VUE_APP_RECAPTCHA_TOKEN,
+      });
+    }
+
+    // Root component
+    vueInstance.mount('#app');
   }
 });
-
-if (process.env.VUE_APP_RECAPTCHA_TOKEN) {
-  Vue.use(VueReCaptcha, {
-    siteKey: process.env.VUE_APP_RECAPTCHA_TOKEN,
-  });
-}
