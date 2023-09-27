72f2379a43230a4372d2f89cc2b1f80617ad2bf2
Tom Russell <TR115251@hotmail.co.uk>
2023-09-27 16:58:03 +0100

    ex0175

--- src/views/Apply/Assessments/SelfAssessmentCompetencies.vue
+++ src/views/Apply/Assessments/SelfAssessmentCompetencies.vue
@@ -31,7 +31,8 @@
               :id="`meet_requirements_${index}`"
               v-model="formData.selectionCriteriaAnswers[index].answer"
               label="Do you meet this requirement?"
-            >
+              :required="vacancy.id === '8GjaA3U51onRf2p1Z2bT'"
+              >
               <RadioItem
                 :value="true"
                 label="Yes"
