3dd23779530deffdd0a60de9357b6c682f3174b0
halcyonjac <halcyon@judicialappointments.digital>
2023-10-18 09:13:07 +0100

    wip

--- src/views/Apply/CommissionerConflicts.vue
+++ src/views/Apply/CommissionerConflicts.vue
@@ -19,6 +19,40 @@
           Are you related to, or known to any of the JAC Commissioners? If you are in any doubt then please select 'Yes' in the list below:
         </p>
 
+        <RadioGroup
+          v-for="(commissioner , index) in formData.additionalInfo.commissionerConflicts"
+          :id="`commissioner-name-${index}`"
+          :key="index"
+          v-model="commissioner.hasRelationship"
+          required
+          :label="commissioner.name"
+        >
+          <RadioItem
+            label="Yes"
+            :value="true"
+          >
+            <TextareaInput
+              id="relationship"
+              v-model="commissioner.detail"
+              label="Please provide details of your relationship."
+              class="govuk-!-width-two-thirds"
+            />
+          </RadioItem>
+          <RadioItem
+            label="No"
+            :value="false"
+          />
+        </RadioGroup>
+
+        <div>
+          <button
+            class="govuk-button govuk-button--secondary"
+            @click="setNoRelationships"
+          >
+            I do not have a pre-existing relationship with any of the commissioners
+          </button>
+        </div>
+
         <button
           :disabled="!canSave(formId)"
           class="govuk-button info-btn--personal-details--save-and-continue"
@@ -35,30 +69,66 @@ import Form from '@/components/Form/Form.vue';
 import ErrorSummary from '@/components/Form/ErrorSummary.vue';
 import ApplyMixIn from './ApplyMixIn';
 import BackLink from '@/components/BackLink.vue';
+import RadioGroup from '@/components/Form/RadioGroup.vue';
+import RadioItem from '@/components/Form/RadioItem.vue';
+import TextareaInput from '@/components/Form/TextareaInput.vue';
 
 export default {
   name: 'CommissionerConflicts',
   components: {
     BackLink,
     ErrorSummary,
+    RadioGroup,
+    RadioItem,
+    TextareaInput,
   },
   extends: Form,
   mixins: [ApplyMixIn],
-  data(){
+  data() {
+    const commissioners = this.$store.state.vacancy.record.commissioners;
     const defaults = {
-      commissionerConflicts: {},
+      additionalInfo: {
+        commissionerConflicts: commissioners.map((commissioner) => ({
+          name: commissioner.name,
+          hasRelationship: null,
+          detail: '',
+        })),
+      },
       progress: {},
     };
+    const formData = { ...defaults };
     const data = this.$store.getters['application/data'](defaults);
-    const formData = { ...defaults, ...data };
+    if (data.additionalInfo.commissionerConflicts) {
+      formData.additionalInfo.commissionerConflicts = data.additionalInfo.commissionerConflicts;
+    }
+    if (data.progress) {
+      formData.progress = data.progress;
+    }
     return {
       formId: 'commissionerConflicts',
-      formData: formData,
+      formData,
     };
   },
   methods: {
     isFormValid() {
-      return true;
+      const commissionerConflicts = this.formData.additionalInfo.commissionerConflicts;
+      const isValid = commissionerConflicts.every((commissioner) => {
+        if (commissioner.hasRelationship && !commissioner.detail) {
+          return false;
+        }
+        return commissioner.hasRelationship !== null;
+      });
+      if (!isValid) {
+        this.errors.push({ id: 'error', message: 'Please provide details of your relationship' });
+      }
+      return isValid;
+    },
+    setNoRelationships(e) {
+      e.preventDefault();
+      this.formData.additionalInfo.commissionerConflicts.forEach((commissioner) => {
+        commissioner.hasRelationship = false;
+        commissioner.detail = '';
+      });
     },
     async save() {
       this.validate();
