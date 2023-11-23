960fa62ffb12c1a913a6fa230cc30a3366a17a1e
warrensearle <warren@precise-minds.co.uk>
2023-11-23 18:27:43 +0000

    #1096 Candidate availability

--- src/components/CandidateFormViews/CandidateAvailability.vue
+++ src/components/CandidateFormViews/CandidateAvailability.vue
@@ -1,6 +1,20 @@
 <template>
   <div>
-    Candidate availability view code goes here
+    <dl class="govuk-summary-list">
+      <dt class="govuk-summary-list__key">
+        You are available on the following days
+      </dt>
+      <dd
+        class="govuk-summary-list__value"
+      >
+        <div
+          v-for="item in localData.availableDays"
+          :key="item"
+        >
+          {{ item.location }}. {{ $filters.formatDate(item.date) }}
+        </div>        
+      </dd>
+    </dl>    
   </div>
 </template>
 
--- src/views/Apply/Forms/Parts/CandidateAvailability.vue
+++ src/views/Apply/Forms/Parts/CandidateAvailability.vue
@@ -6,10 +6,19 @@
         Candidate Availability
       </h1>
 
-      <input 
-        v-model="formData.test" 
-        type="text"
+      <CheckboxGroup
+        id="availability"
+        v-model="formData.availableDays"
+        label="Please tick all dates you are available"
+        required
       >
+        <CheckboxItem
+          v-for="item in selectionDays"
+          :key="item"
+          :value="item"
+          :label="`${item.location}. ${$filters.formatDate(item.date)}`"
+        />
+      </CheckboxGroup>
 
       <button
         class="govuk-button info-btn--candidate-availability--save-and-continue"
@@ -24,12 +33,35 @@
 import { APPLICATION_FORM_PARTS } from '@/helpers/constants';
 import BackLink from '@/components/BackLink.vue';
 import CandidateFormsMixIn from '@/views/Apply/Forms/CandidateFormsMixIn';
+import { dateRange } from '@/helpers/date';
+import CheckboxGroup from '@/components/Form/CheckboxGroup.vue';
+import CheckboxItem from '@/components/Form/CheckboxItem.vue';
+
 export default {
   name: 'CandidateFormCandidateAvailability',
   components: {
     BackLink,
+    CheckboxGroup,
+    CheckboxItem,
   },
   mixins: [CandidateFormsMixIn],
+  computed: {
+    selectionDays() {
+      const options = [];
+      if (!this.vacancy) return options;
+      if (!this.vacancy.selectionDays) return options;
+      this.vacancy.selectionDays.forEach(selectionDay => {
+        const dates = dateRange(selectionDay.selectionDayStart, selectionDay.selectionDayEnd);
+        dates.forEach(item => {
+          options.push({
+            location: selectionDay.selectionDayLocation,
+            date: item,
+          });
+        });
+      });
+      return options;
+    },
+  },
   created() {
     this.setupPart(APPLICATION_FORM_PARTS.CANDIDATE_AVAILABILITY, true);
   },
