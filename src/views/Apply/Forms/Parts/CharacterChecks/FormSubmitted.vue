767ea7a7ca8c6f37d868cc3f1b18d2c14bfb0099
HalcyonJAC <79906532+HalcyonJAC@users.noreply.github.com>
2023-12-01 14:26:06 +0000

    Feature/1089 Transfer character checks to PSD (#1113)
    * copy character checks files
    
    * update router
    
    * fix entry page of chacracter checks
    
    * update path name
    
    * keep original character checks flow
    
    * ensure vacancy id exists
    
    * fix router name
    
    * update review page

---
+++ src/views/Apply/Forms/Parts/CharacterChecks/FormSubmitted.vue
@@ -0,0 +1,59 @@
+<!-- comment out below until we get feedback from users -->
+<!-- <template>
+  <div class="govuk-grid-column-two-thirds govuk-!-margin-top-8">
+    <div class="govuk-panel govuk-panel--confirmation">
+      <h1 class="govuk-panel__title">
+        Consent to character checks submitted
+      </h1>
+    </div>
+    <h2 class="govuk-heading-m govuk-!-margin-top-6">
+      What happens next
+    </h2>
+    <p class="govuk-body">
+      This information will be used by the Judicial Appointments Commission to undertake character checks.
+    </p>
+    <p class="govuk-body">
+      We may get in touch to ask for more information. In the meantime refer to the timeline, given in your exercise to understand the next steps ahead.
+    </p>
+    <p class="govuk-body">
+      If your circumstances change after submitting this form that might impact your ability to meet the good character requirements please notify the
+      <a
+        :href="`mailto:${vacancy.exerciseMailbox}?subject=Re: ${vacancy.referenceNumber}`"
+        class="govuk-link"
+      >Selection Exercise Team</a>.
+    </p>
+    <p class="govuk-body">
+      Any personal data collected and processed is in accordance with the <a
+        href="https://judicialappointments.gov.uk/data-protection-freedom-of-information-and-making-requests-for-your-data/"
+        class="govuk-link"
+        target="_blank"
+      >JAC's privacy standards</a>.
+    </p>
+    <p class="govuk-body govuk-!-margin-top-6">
+      <RouterLink
+        :to="{ name: 'applications' }"
+      >
+        Return to your applications
+      </RouterLink>
+    </p>
+  </div>
+</template> -->
+
+<script>
+import CandidateFormsMixIn from '@/views/Apply/Forms/CandidateFormsMixIn';
+import { APPLICATION_FORM_PARTS } from '@/helpers/constants';
+
+export default {
+  name: 'FormSubmitted',
+  mixins: [CandidateFormsMixIn],
+  computed: {
+    vacancy() {
+      return this.$store.state.vacancy.record;
+    },
+  },
+  created() {
+    this.setupPart(APPLICATION_FORM_PARTS.CHARACTER_CHECKS);
+    this.save();
+  },
+};
+</script>
