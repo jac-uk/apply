0c4e2c1ffbcc34b773381f9e9a89c8e919d71397
Tom Russell <TR115251@hotmail.co.uk>
2023-12-04 17:28:08 +0000

    reconfirm welsh posts

--- src/components/CandidateFormViews/WelshPosts.vue
+++ src/components/CandidateFormViews/WelshPosts.vue
@@ -1,7 +1,61 @@
 <template>
-  <div>
-    Welsh posts view code goes here
-  </div>
+  <dl
+    v-if="application"
+    class="govuk-summary-list"
+  >
+    <div
+      v-if="application.applyingForWelshPost != null"
+      class="govuk-summary-list__row"
+    >
+      <dt class="govuk-summary-list__key">
+        Are you applying for a Welsh post?
+      </dt>
+      <dd
+        class="govuk-summary-list__value"
+        data-welsh="applying-for-welsh-post"
+      >
+        {{ $filters.toYesNo(application.applyingForWelshPost) }}
+      </dd>
+    </div>
+    <div
+      v-if="application.canSpeakWelsh != null"
+      class="govuk-summary-list__row"
+    >
+      <dt class="govuk-summary-list__key">
+        Can you speak Welsh?
+      </dt>
+      <dd
+        class="govuk-summary-list__value"
+        data-welsh="speak-welsh"
+      >
+        {{ $filters.toYesNo(application.canSpeakWelsh) }}
+      </dd>
+    </div>
+    <div
+      v-if="application.canReadAndWriteWelsh != null"
+      class="govuk-summary-list__row"
+    >
+      <dt class="govuk-summary-list__key">
+        Can you read and write in Welsh?
+      </dt>
+      <dd
+        class="govuk-summary-list__value"
+      >
+        <p
+          v-if="application.canReadAndWriteWelsh == false"
+          data-welsh="read-and-write-welsh"
+        >
+          {{ $filters.toYesNo(application.canReadAndWriteWelsh) }}
+        </p>
+        <p
+          v-if="application.canReadAndWriteWelsh"
+          data-welsh="read-and-write-welsh"
+        >
+          {{ $filters.lookup(application.canReadAndWriteWelsh) }}
+        </p>
+      </dd>
+    </div>
+  </dl>
 </template>
 
 <script>
--- src/views/Apply/Forms/Parts/WelshPosts.vue
+++ src/views/Apply/Forms/Parts/WelshPosts.vue
@@ -5,6 +5,72 @@
       <h1 class="govuk-heading-xl">
         Welsh Posts
       </h1>
+
+      <p class="govuk-body-m govuk-!-margin-top-0">
+        Please update this section to take into account any changes since you submitted your application
+      </p>
+
+      <RadioGroup
+        v-if="isWelshAdministrationRequired"
+        id="applying-for-welsh-post"
+        v-model="formData.applyingForWelshPost"
+        required
+        label="Are you applying for a post in Wales?"
+      >
+        <RadioItem
+          :value="true"
+          label="Yes"
+        />
+        <RadioItem
+          :value="false"
+          label="No"
+        />
+      </RadioGroup>
+
+      <RadioGroup
+        v-if="isSpeakWelshRequired"
+        id="speak-welsh"
+        v-model="formData.canSpeakWelsh"
+        required
+        label="Can you speak Welsh?"
+        hint="You will be tested on this later in the process."
+      >
+        <RadioItem
+          :value="true"
+          label="Yes"
+        />
+        <RadioItem
+          :value="false"
+          label="No"
+        />
+      </RadioGroup>
+
+      <RadioGroup
+        v-if="isReadWriteWelshRequired"
+        id="read-and-write-welsh"
+        v-model="formData.canReadAndWriteWelsh"
+        required
+        label="Can you read and write in Welsh?"
+        hint="You will be tested on this later in the process."
+      >
+        <RadioItem
+          :value="false"
+          label="No"
+        />
+        <RadioItem
+          value="read"
+          label="Read"
+        />
+        <RadioItem
+          value="write"
+          label="Write"
+        />
+        <RadioItem
+          value="both"
+          label="Both"
+        />
+      </RadioGroup>
+
       <button
         class="govuk-button info-btn--candidate-availability--save-and-continue"
         @click="save()"
@@ -18,14 +84,60 @@
 import { APPLICATION_FORM_PARTS } from '@/helpers/constants';
 import BackLink from '@/components/BackLink.vue';
 import CandidateFormsMixIn from '@/views/Apply/Forms/CandidateFormsMixIn';
+import RadioGroup from '@/components/Form/RadioGroup.vue';
+import RadioItem from '@/components/Form/RadioItem.vue';
+
+import {
+  isWelshAdministrationRequired,
+  isSpeakWelshRequired,
+  isReadWriteWelshRequired,
+  isApplyingForWelshPost
+} from '@/helpers/exerciseHelper';
+
 export default {
   name: 'CandidateFormWelshPosts',
   components: {
+    RadioGroup,
+    RadioItem,
     BackLink,
   },
   mixins: [CandidateFormsMixIn],
+  data() {
+    const application = this.$store.getters['application/data']();
+    const formData = {
+      canReadAndWriteWelsh: application.canReadAndWriteWelsh,
+      canSpeakWelsh: application.canSpeakWelsh,
+      applyingForWelshPost: application.applyingForWelshPost,
+    };
+    return {
+      formData,
+    };
+  },
   created() {
     this.setupPart(APPLICATION_FORM_PARTS.WELSH_POSTS);
+  },
+  methods: {
+    isWelshAdministrationRequired() {
+      return isWelshAdministrationRequired(this.vacancy);
+    },
+    isSpeakWelshRequired() {
+      return isSpeakWelshRequired(this.vacancy);
+    },
+    isReadWriteWelshRequired() {
+      return isReadWriteWelshRequired(this.vacancy);
+    },
+    isApplyingForWelshPost() {
+      return isApplyingForWelshPost(this.vacancy, this.application);
+    },
+    async save(){
+      const saveData = {
+        canReadAndWriteWelsh: this.formData.canReadAndWriteWelsh,
+        canSpeakWelsh: this.formData.canSpeakWelsh,
+        applyingForWelshPost: this.formData.applyingForWelshPost,
+      };
+      await this.$store.dispatch('application/save', saveData);
+      await this.savePart(true);
+    },
   },
 };
 </script>
