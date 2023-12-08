ed070c5cb5d0fdbce707edad80db10787acc8423
halcyonjac <halcyon@judicialappointments.digital>
2023-12-08 16:03:34 +0000

    remove required

--- src/views/Apply/QualificationsAndExperience/RelevantMemberships.vue
+++ src/views/Apply/QualificationsAndExperience/RelevantMemberships.vue
@@ -17,7 +17,6 @@
           id="professional-memberships"
           v-model="formData.professionalMemberships"
           label="What professional memberships do you have?"
-          :required="isJAC00187"
         >
           <CheckboxItem
             v-if="showMembershipOption('chartered-association-of-building-engineers')"
