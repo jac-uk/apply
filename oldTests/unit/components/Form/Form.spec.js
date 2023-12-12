c5f79489a264973339dbfa4e2417b6f0c02638c5
drieJac <jac.drie@digitalteam.uk>
2023-12-12 13:16:48 +0000

    Uncommented tests in the oldTests directory

--- oldTests/unit/components/Form/Form.spec.js
+++ oldTests/unit/components/Form/Form.spec.js
@@ -1,24 +1,21 @@
-// import { createTestSubject } from '../../helpers';
+import { createTestSubject } from '../../helpers';
 
-// import Form from '@/components/Form/Form.vue';
-// //@FIXME@ Testing this suite requires the ref 'formRef' to be mocked
-// describe.skip('components/Form/Form', () => {
-//     describe('component instance', () => {
-//         let wrapper;
-//         beforeEach(() => {
-//         wrapper = createTestSubject(Form, {
-//             mocks: {},
-//             stubs: [],
-//             propsData: {},
-//             });
-//         });
+import Form from '@/components/Form/Form.vue';
+//@FIXME@ Testing this suite requires the ref 'formRef' to be mocked
+describe.skip('components/Form/Form', () => {
+    describe('component instance', () => {
+        let wrapper;
+        beforeEach(() => {
+        wrapper = createTestSubject(Form, {
+            mocks: {},
+            stubs: [],
+            propsData: {},
+            });
+        });
 
-//         it('renders the component', () => {
-//             expect(wrapper.exists()).toBe(true);
-//         });
-//     });
-// });
-
-it('empty suite', () => {
-
+        it('renders the component', () => {
+            expect(wrapper.exists()).toBe(true);
+        });
+    });
 });
+
--- oldTests/unit/views/Apply/QualificationsAndExperience/RelevantMemberships.spec.js
+++ oldTests/unit/views/Apply/QualificationsAndExperience/RelevantMemberships.spec.js
@@ -1,27 +1,23 @@
-// // @FIXME@ *form/queryselector* this suite somehow interacts with the Form
-// //  component and errors due to the queryselector
+// @FIXME@ *form/queryselector* this suite somehow interacts with the Form
+//  component and errors due to the queryselector
 
-// import RelevantMemberships from '@/views/Apply/QualificationsAndExperience/RelevantMemberships.vue';
-// import { createTestSubject } from '../../../helpers';
+import RelevantMemberships from '@/views/Apply/QualificationsAndExperience/RelevantMemberships.vue';
+import { createTestSubject } from '../../../helpers';
 
-// describe.skip('@/views/Apply/QualificationsAndExperience/RelevantMemberships', () => {
-//     describe('template', () => {
-//         let wrapper;
-//         beforeEach(() => {
-//             wrapper = createTestSubject(RelevantMemberships, {
-//                 mocks: {},
-//                 stubs: [],
-//                 propsData: {},
-//             });
-//         });
+describe.skip('@/views/Apply/QualificationsAndExperience/RelevantMemberships', () => {
+    describe('template', () => {
+        let wrapper;
+        beforeEach(() => {
+            wrapper = createTestSubject(RelevantMemberships, {
+                mocks: {},
+                stubs: [],
+                propsData: {},
+            });
+        });
 
-//         it('renders', () => {
-//             expect(wrapper.exists()).toBe(true);
-//         });
+        it('renders', () => {
+            expect(wrapper.exists()).toBe(true);
+        });
 
-//     });
-// });
-
-it('empty suite', () => {
-
+    });
 });
