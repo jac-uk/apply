33925dc666c354901f05b6f45fbafebccca9638a
warrensearle <warren@precise-minds.co.uk>
2023-12-14 18:03:59 +0000

    Remove realtime database and unused session

--- src/firebase.js
+++ src/firebase.js
@@ -3,7 +3,6 @@ import 'firebase/auth';
 import 'firebase/firestore';
 import 'firebase/functions';
 import 'firebase/storage';
-import 'firebase/database';
 import 'firebase/app-check';
 
 // Configure and initialise Firebase
@@ -21,7 +20,6 @@ const functions = firebase.initializeApp(config).functions('europe-west2');
 const firestore = firebase.firestore();
 const auth = firebase.auth();
 const storage = firebase.storage();
-const database = firebase.database();
 const Timestamp = firebase.firestore.Timestamp;
 
 // Local emulator
@@ -30,7 +28,6 @@ if (location.hostname === 'localhost' && import.meta.env.VITE_FIREBASE_USE_EMULA
   functions.useEmulator('localhost', 5001);
   auth.useEmulator('http://localhost:9099');
   storage.useEmulator('localhost', 9199);
-  database.useEmulator('localhost', 9000);
 }
 
 // App check
@@ -38,5 +35,5 @@ if (import.meta.env.VITE_RECAPTCHA_TOKEN) {
   firebase.appCheck().activate(import.meta.env.VITE_RECAPTCHA_TOKEN);
 }
 
-export { firestore, auth, functions, database, storage, Timestamp };
+export { firestore, auth, functions, storage, Timestamp };
 export default firebase;
