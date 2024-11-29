1149bd48549eb3655b915c17930c2e1435fb610d
drieJAC <jac.drie@digitalteam.uk>
2024-11-29 15:19:13 +0000

    #1250 4.15 Password validation (#1268)
    * Added current and confirm pwd fields.
    Added mappings to map some of the error codes sent by firebase to user friendly ones.
    Moved sign in call to the auth service.
    
    * Adding mapping and message for incorrect current pwd
    
    * Remove length condition from currentPassword field

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,4 +1,4 @@
-import { createUserWithEmailAndPassword, sendEmailVerification, sendPasswordResetEmail, getAuth } from 'firebase/auth';
+import { createUserWithEmailAndPassword, sendEmailVerification, sendPasswordResetEmail, getAuth, signInWithEmailAndPassword, updatePassword } from 'firebase/auth';
 
 const createAuthUser = async (email, password) => {
   const auth = getAuth();
@@ -31,9 +31,45 @@ const sendPasswordResetLink = async (email, returnUrl) => {
   });
 };
 
+const signInWithEmailAndPwd = async (email, password) => {
+  const auth = getAuth();
+  return await signInWithEmailAndPassword(
+    auth,
+    email,
+    password
+  );
+};
+
+const  updatePwd = async(currentPwd, newPwd) => {
+  const user = await getAuthUser();
+  await signInWithEmailAndPwd(
+    user.email,
+    currentPwd
+  );
+  await updatePassword(user, newPwd);
+};
+
+const mapPwdResetMessage = (errorCode) => {
+  const errorMessages = {
+    //'CREDENTIAL_TOO_OLD_LOGIN_AGAIN'
+    'auth/invalid-credential': 'The current password is incorrect.',
+    'auth/wrong-password': 'The current password is incorrect.',
+    'auth/weak-password': 'The new password is too weak. Please choose a stronger password.',
+    'auth/too-many-requests': 'Too many attempts. Please try again later.',
+    'auth/network-request-failed': 'Network error. Please check your internet connection.',
+    'auth/requires-recent-login': 'Your session is too old. Please log in again to perform this action.',
+    default: 'An unexpected error occurred. Please try again or contact admin.',
+  };
+
+  return errorMessages[errorCode] || errorMessages.default;
+};
+
 export {
   createAuthUser,
   getAuthUser,
   sendEmailVerificationLink,
-  sendPasswordResetLink
+  sendPasswordResetLink,
+  signInWithEmailAndPwd,
+  mapPwdResetMessage,
+  updatePwd
 };
