43d7f2b4145ea291a057080b206ab4afd09f9d92
Ryan Hsieh <79906532+HalcyonJAC@users.noreply.github.com>
2025-01-03 11:06:43 +0000

    Bugfix/TS 496 Self assessment extraction word counts (#1275)
    * upgrade jac-kit to 4.1.51
    
    * fix vulnerabilities
    
    * upgrade jac-kit to 4.1.52
    
    * update package-lock.json
    
    * use splitWords from jac-kit
    
    * upgrade jac-kit to 4.1.53
    
    * upgrade jac-kit to 4.1.53

--- src/components/Form/FormField.vue
+++ src/components/Form/FormField.vue
@@ -5,6 +5,7 @@
 <script>
 import { isValidNumberForRegion } from 'libphonenumber-js';
 import { formatDate } from '@/helpers/date';
+import { splitWords } from '@jac-uk/jac-kit/helpers/splitWords';
 
 export default {
   name: 'FormField',
@@ -79,17 +80,7 @@ export default {
     words() {
       const value = this.modelValue;
       const result = value ? value : '';
-      return [].concat(...result
-        .split(/[^a-z'-]/i) //split into array at every occurance of a character which is NOT: a-z or ' or -
-        .filter(item => item != '') // remove any empty items from array
-        .filter(item => item != '\'') // remove any items which are just a apostrophe
-        .filter(item => item != '-') // remove any items which are just a hyphen
-        .map((item, i) => {                                           // with the above array
-          if (i, item.replace(/[^-]/g, '').length >= 4) {             // find any items containing more than or equal to 4 hyphens (4 allows for a trailing hyphen which is not counted in next set)
-            item = item.match(/((?:[^-]*?-){3}[^-]*?)-|([\S\s]+)/g);  // if an 'offending' item occurs, group every 4 words, ignoring the hyphen between groups [ie. 'one-one-one-one-two-two-two-two' (eight words, seven hyphens) 'one-one-one-one-' 'two-two-two-two']
-          }
-          return item; // add array in position of word
-        })); // flatten array
+      return splitWords(result);
     },
   },
   mounted: function () {
